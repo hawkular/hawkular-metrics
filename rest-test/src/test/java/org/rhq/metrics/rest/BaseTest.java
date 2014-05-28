3a1197ad4ab18072fc32ec4203922702ddfeeb82
Heiko W. Rupp <hwr@pilhuhn.de>
2014-05-28 13:25:20 +0200

    Add the Arquillian machinery to startup a Wildfly 8 server and run tests against the rest-api.

--- rest-test/src/test/java/org/rhq/metrics/rest/BaseTest.java
+++ rest-test/src/test/java/org/rhq/metrics/rest/BaseTest.java
@@ -1,24 +1,58 @@
 package org.rhq.metrics.rest;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
 
+@RunWith(Arquillian.class)
 public class BaseTest {
 
+    private static final int HTTP_PORT = 58080;
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        File pomFile = new File("../rest-servlet/pom.xml");
+        System.out.println("pomfile " + pomFile.getAbsolutePath());
+        System.out.flush();
+
+        WebArchive archive =
+        ShrinkWrap.create(MavenImporter.class)
+            .offline()
+            .loadPomFromFile(pomFile)
+            .importBuildOutput()
+            .as(WebArchive.class);
+        System.out.println("archive is " + archive.toString(false));
+        System.out.flush();
+        return archive;
+    }
+
     @Test
+    @RunAsClient
     public void pingTest() throws Exception {
         Response jsonp = given()
+                .port(HTTP_PORT)
                 .expect()
                     .statusCode(200)
                 .when()
@@ -30,12 +64,46 @@ public class BaseTest {
 
         JsonPath jsonPath = new JsonPath(jsonp.asString());
 
-        DateFormat df = new SimpleDateFormat("EEE MMM d H:mm:ss z yyyy");
+        DateFormat df = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US);
         Date date = df.parse(jsonPath.getString("pong"));
         Date now = new Date();
 
         long timeDifference = now.getTime() - date.getTime();
 
-        Assert.assertTrue(timeDifference < 1000L);
+        Assert.assertTrue("Difference is " + timeDifference, timeDifference < 2500L);
+    }
+
+    @Test
+    @RunAsClient
+    public void testAddGetValue() throws Exception {
+
+        Map<String,Object> data = new HashMap<>();
+        String id = "foo";
+        data.put("id", id);
+        long now = System.currentTimeMillis();
+        data.put("timestamp", now);
+        data.put("value",42d);
+
+        given()
+            .port(HTTP_PORT)
+            .body(data)
+            .pathParam("id",id)
+            .contentType(ContentType.JSON)
+        .expect()
+            .statusCode(200)
+        .when()
+            .post("/rhq-metrics/metrics/{id}");
+
+
+        given()
+            .port(HTTP_PORT)
+            .pathParam("id", id)
+          .header("Accepts", "application/json")
+        .expect()
+           .statusCode(200)
+            .log().ifError()
+            .body("timestamp[0]", equalTo(now))
+        .when()
+           .get("/rhq-metrics/metrics/{id}");
     }
 }
