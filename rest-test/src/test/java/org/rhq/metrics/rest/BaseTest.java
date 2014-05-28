528de7491b4e9126113e223573c207c04cd67596
Heiko W. Rupp <hwr@pilhuhn.de>
2014-05-28 20:54:41 +0200

    Set the injected URI as base for the whole test class. Add a test for add/get multiple items

--- rest-test/src/test/java/org/rhq/metrics/rest/BaseTest.java
+++ rest-test/src/test/java/org/rhq/metrics/rest/BaseTest.java
@@ -2,13 +2,16 @@ package org.rhq.metrics.rest;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 import java.io.File;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -19,9 +22,11 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
@@ -32,7 +37,8 @@ public class BaseTest {
     @Deployment(testable=false)
     public static WebArchive createDeployment() {
         File pomFile = new File("../rest-servlet/pom.xml");
-        System.out.println("pomfile " + pomFile.getAbsolutePath());
+
+        System.out.println("Pom file path: " + pomFile.getAbsolutePath());
         System.out.flush();
 
         WebArchive archive =
@@ -49,13 +55,21 @@ public class BaseTest {
     @ArquillianResource
     private URL baseUrl;
 
+    @Before
+    public void setupRestAssured() {
+        RestAssured.baseURI = baseUrl.toString();
+        // There is no need to set RestAssured.basePath as this is already in the baseUrl,
+        // set via Arquillian in the baseUrl
+    }
+
+
     @Test
     public void pingTest() throws Exception {
         Response jsonp = given()
                 .expect()
                     .statusCode(200)
                 .when()
-                    .post(new URL(baseUrl, "/rhq-metrics/ping"))
+                    .post("/ping")
                 .then()
                     .contentType(ContentType.JSON)
                 .extract()
@@ -75,12 +89,9 @@ public class BaseTest {
     @Test
     public void testAddGetValue() throws Exception {
 
-        Map<String,Object> data = new HashMap<>();
         String id = "foo";
-        data.put("id", id);
         long now = System.currentTimeMillis();
-        data.put("timestamp", now);
-        data.put("value",42d);
+        Map<String,Object> data = createDataPoint(id, now, 42d);
 
         given()
             .body(data)
@@ -89,17 +100,61 @@ public class BaseTest {
         .expect()
             .statusCode(200)
         .when()
-            .post(new URL(baseUrl, "/rhq-metrics/metrics/{id}"));
+            .post("/metrics/{id}");
+
+
+        given()
+            .pathParam("id", id)
+            .header("Accepts", "application/json")
+        .expect()
+            .statusCode(200)
+            .log().ifError()
+            .body("timestamp[0]", equalTo(now))
+        .when()
+           .get(new URL(baseUrl, "/rhq-metrics/metrics/{id}"));
+    }
+
+    @Test
+    public void testAddGetValues() throws Exception {
+
+        List<Map<String,Object>> data = new ArrayList<>();
+        String id = "bla";
+        long now = System.currentTimeMillis();
+
+        Map<String,Object> data1 = createDataPoint(id,now-10,42d);
+        Map<String,Object> data2 = createDataPoint(id,now,4242d);
+        data.add(data1);
+        data.add(data2);
+
+        given()
+            .body(data)
+            .contentType(ContentType.JSON)
+        .expect()
+            .statusCode(200)
+        .when()
+            .post("/metrics");
 
 
         given()
             .pathParam("id", id)
           .header("Accepts", "application/json")
+            .queryParam("start",now-15)
+            .queryParam("end",now+5)
         .expect()
            .statusCode(200)
             .log().ifError()
-            .body("timestamp[0]", equalTo(now))
+            .body("", hasSize(2))
         .when()
-           .get(new URL(baseUrl, "/rhq-metrics/metrics/{id}"));
+           .get("/metrics/{id}");
+    }
+
+    private Map<String, Object> createDataPoint(String id, long time, Double value) {
+        Map<String,Object> data = new HashMap<>(3);
+        data.put("id", id);
+        data.put("timestamp", time);
+        data.put("value",value);
+
+        return data;
+
     }
 }
