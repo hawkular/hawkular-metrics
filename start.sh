f203452cc1ac3d77c0a8c268c403c159343f21a3
Heiko W. Rupp <hwr@pilhuhn.de>
2014-06-01 11:08:22 +0200

    Improve the Quickstart and make it download WildFly if needed.

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ BACKEND=mem
 # uncomment the next line if you want to use an already running Cassandra server for the backend.
 # BACKEND=cass
 
+WFLY_VERSION=`grep "<version.wildfly>" pom.xml | sed -E 's/^.*y>(8.*l)<.*$/\1/'`
+
 if [ ! -e core-api/target/rhq-metrics-api*.jar ]
 then
     cd core-api
@@ -39,13 +41,19 @@ then
     mkdir target
 fi
 
+if [ ! -e $HOME/.m2/repository/org/wildfly/wildfly-dist/8.0.0.Final/wildfly-dist-$WFLY_VERSION.zip ]
+then
+    echo "Downloading WildFly $WFLY_VERSION into local maven repository"
+    mvn -P download_wildfly -DskipTests install
+fi
+
 if [ ! -e target/wild* ]
 then
     cd target
-    unzip $HOME/.m2/repository/org/wildfly/wildfly-dist/8.0.0.Final/wildfly-dist-8.0.0.Final.zip
+    unzip $HOME/.m2/repository/org/wildfly/wildfly-dist/8.0.0.Final/wildfly-dist-$WFLY_VERSION.zip
     cd ..
 fi
 
-cp rest-servlet/target/rhq-metric-rest*.war target/wildfly-8.0.0.Final/standalone/deployments/
-cp dummy-ui/target/dummy-ui*.war target/wildfly-8.0.0.Final/standalone/deployments/
-target/wildfly-8.0.0.Final/bin/standalone.sh -Drhq-metrics.backend=$BACKEND --debug 8787
+cp rest-servlet/target/rhq-metric-rest*.war target/wildfly-$WFLY_VERSION/standalone/deployments/
+cp dummy-ui/target/dummy-ui*.war target/wildfly-$WFLY_VERSION/standalone/deployments/
+target/wildfly-$WFLY_VERSION/bin/standalone.sh -Drhq-metrics.backend=$BACKEND --debug 8787
