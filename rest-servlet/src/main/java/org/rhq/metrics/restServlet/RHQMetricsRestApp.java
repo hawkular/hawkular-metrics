0ac494282408c300ecb585444b8fa51b7812b840
Heiko W. Rupp <hwr@pilhuhn.de>
2014-06-07 19:10:27 +0200

    Create profiles to select backend for tests.

--- rest-servlet/src/main/java/org/rhq/metrics/restServlet/RHQMetricsRestApp.java
+++ rest-servlet/src/main/java/org/rhq/metrics/restServlet/RHQMetricsRestApp.java
@@ -1,5 +1,6 @@
 package org.rhq.metrics.restServlet;
 
+import javax.inject.Inject;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
@@ -12,7 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.rhq.metrics.core.MetricsService;
 
 /**
- * Rest app initalization
+ * Rest app initialization
  * @author Heiko W. Rupp
  */
 @ApplicationPath("/")
@@ -20,41 +21,13 @@ public class RHQMetricsRestApp extends Application {
 
     private static final Logger logger = LoggerFactory.getLogger(RHQMetricsRestApp.class);
 
+    @Inject
     private MetricsService metricsService;
 
-    public RHQMetricsRestApp(@Context ServletConfig servletConfig) {
+    public RHQMetricsRestApp() {
 
         logger.info("RHQ Metrics starting ..");
 
-        ServletContext servletContext = servletConfig.getServletContext();
-        String backendClassName = servletContext.getInitParameter("rhq-metrics.backend");
-
-        logger.info(".. using a backend of  " + backendClassName);
-
-//        try {
-//            @SuppressWarnings("rawtypes")
-//            Class clazz = Class.forName(backendClassName);
-//
-//            metricsService = (MetricsService) clazz.newInstance();
-//            Map<String,String> params = new HashMap<>();
-//            Enumeration<String> initParameterNames = servletContext.getInitParameterNames();
-//            while (initParameterNames.hasMoreElements()) {
-//                String name = initParameterNames.nextElement();
-//                params.put(name, servletContext.getInitParameter(name));
-//            }
-//            metricsService.startUp(params);
-//        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-//            logger.error("Can not start the backend:", e);
-//            throw new RuntimeException("Startup failed");
-//        }
-//        ServiceKeeper.getInstance().service = metricsService;
-
     }
-
-    // TODO Figure out why this does not work. Deployment fails with a CDI exception
-//    @Produces
-//    public MetricsService getMetricsService() {
-//        return metricsService;
-//    }
 
 }
