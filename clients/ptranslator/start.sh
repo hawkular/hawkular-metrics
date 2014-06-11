94a610c7561d5420ab92833fd80db622b3ac6fc4
Heiko W. Rupp <hwr@pilhuhn.de>
2014-06-11 16:50:20 +0200

    Update ptrans jar version to 0.2.0. Need an automatic way to do this.

--- clients/ptranslator/start.sh
+++ clients/ptranslator/start.sh
@@ -3,7 +3,7 @@
 java -Djava.net.preferIPv4Stack=true \
    -cp ${HOME}/.m2/repository/io/netty/netty-all/4.0.19.Final/netty-all-4.0.19.Final.jar\
 :ptrans.properties\
-:target/ptrans-0.1.0-SNAPSHOT.jar\
+:target/ptrans-0.2.0-SNAPSHOT.jar\
 :${HOME}/.m2/repository/org/slf4j/slf4j-log4j12/1.7.7/slf4j-log4j12-1.7.7.jar\
 :${HOME}/.m2/repository/org/slf4j/slf4j-api/1.7.7/slf4j-api-1.7.7.jar\
 :${HOME}/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar\
