b78d949bd8b38233cb1550588d127fd4914012e2
Heiko W. Rupp <hwr@pilhuhn.de>
2014-06-27 21:38:08 +0200

    Bump version of jar [no ci]

--- clients/ptranslator/start.sh
+++ clients/ptranslator/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d target -o ! -f target/ptrans-0.2.0-SNAPSHOT.jar ]
+if [ ! -d target -o ! -f target/ptrans-0.2.1-SNAPSHOT.jar ]
 then
     mvn install
 fi
@@ -8,7 +8,7 @@ fi
 java -Djava.net.preferIPv4Stack=true \
    -cp ${HOME}/.m2/repository/io/netty/netty-all/4.0.19.Final/netty-all-4.0.19.Final.jar\
 :ptrans.properties\
-:target/ptrans-0.2.0-SNAPSHOT.jar\
+:target/ptrans-0.2.1-SNAPSHOT.jar\
 :${HOME}/.m2/repository/org/slf4j/slf4j-log4j12/1.7.7/slf4j-log4j12-1.7.7.jar\
 :${HOME}/.m2/repository/org/slf4j/slf4j-api/1.7.7/slf4j-api-1.7.7.jar\
 :${HOME}/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar\
