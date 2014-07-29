fa4c1f756f6e4ed4b04450baf4df81f196ceb9e7
Heiko W. Rupp <hwr@pilhuhn.de>
2014-07-29 15:08:40 +0200

    Get the version from the pom file. Assumes the pom file structure stays for the top few lines so that the module version is the 3rd occurrence of <version>

--- clients/ptranslator/start.sh
+++ clients/ptranslator/start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-if [ ! -d target -o ! -f target/ptrans-0.2.1-SNAPSHOT.jar ]
+
+VERSION=`grep version pom.xml | sed -n '3,3p' | sed -E 's/^.*n>(.*)<.*$/\1/' `
+
+
+if [ ! -d target -o ! -f target/ptrans-$VERSION ]
 then
     mvn install
 fi
@@ -8,7 +12,7 @@ fi
 java -Djava.net.preferIPv4Stack=true \
    -cp ${HOME}/.m2/repository/io/netty/netty-all/4.0.20.Final/netty-all-4.0.20.Final.jar\
 :ptrans.properties\
-:target/ptrans-0.2.1-SNAPSHOT.jar\
+:target/ptrans-$VERSION.jar\
 :${HOME}/.m2/repository/org/slf4j/slf4j-log4j12/1.7.7/slf4j-log4j12-1.7.7.jar\
 :${HOME}/.m2/repository/org/slf4j/slf4j-api/1.7.7/slf4j-api-1.7.7.jar\
 :${HOME}/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar\
