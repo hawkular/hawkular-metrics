febef6775c5366d14b8c2c569f3fa789b6157323
Mike Thompson <mithomps@redhat.com>
2014-08-05 16:37:11 -0700

    Refactor the controller to standalone functions in accordance with best practices: https://github.com/toddmotto/angularjs-styleguide

--- console/src/main/webapp/scripts/controllers/global-controller.js
+++ console/src/main/webapp/scripts/controllers/global-controller.js
@@ -5,23 +5,19 @@
 /**
  * @ngdoc controller
  * @name GlobalController
- * @description This controller is responsible for changing servers to connect to.
+ * @description This controller is responsible for changing servers to connect.
  */
+function GlobalController($rootScope, $localStorage ) {
+    var vm = this;
+    // NOTE: the $rootScope.$storage.server is setup in app.js run module;
+    // $rootScope is needed here because it is accessible to run module as controller scopes are not
+
+    vm.saveServerPort = function(){
+        $localStorage.server = $rootScope.$storage.server;
+        $localStorage.port = $rootScope.$storage.port;
+
+    };
+}
+
 angular.module('chartingApp')
-    .controller('GlobalController', ['$log','$rootScope', '$localStorage', function ($log, $rootScope, $localStorage ) {
-        var vm = this;
-        // NOTE: the $rootScope.$storage.server is setup in app.js run module;
-        // $rootScope is needed here because it is accessible to run module as controller scopes are not
-
-//        vm.$storage = $localStorage.$default({
-//            server: 'localhost',
-//            port: '8080'
-//        });
-
-        vm.saveServerPort = function(){
-            $localStorage.server = $rootScope.$storage.server;
-            $localStorage.port = $rootScope.$storage.port;
-
-        };
-    }
-]);
+    .controller('GlobalController', ['$rootScope', '$localStorage', GlobalController]);
--- console/src/main/webapp/scripts/controllers/insert-metrics-controller.js
+++ console/src/main/webapp/scripts/controllers/insert-metrics-controller.js
@@ -7,154 +7,155 @@
  * @description A controller for inserting metrics into the rhq-metrics data store (either in-memory or Cassandra).
  *
  */
-angular.module('chartingApp')
-    .controller('InsertMetricsController', ['$scope', '$rootScope', '$log','$interval', 'metricDataService', function ($scope, $rootScope, $log, $interval, metricDataService) {
-        var streamingIntervalPromise, 
-            vm = this;
+function InsertMetricsController($scope, $rootScope, $log, $interval, metricDataService) {
+    var streamingIntervalPromise,
+        vm = this;
 
-        $rootScope.showOpenGroup = true;
+    $rootScope.showOpenGroup = true;
 
-        vm.streamingTimeRanges = [
-            { "range": "1s", "rangeInSeconds": 1 },
-            { "range": "5s", "rangeInSeconds": 5 },
-            { "range": "30s", "rangeInSeconds": 30 },
-            { "range": "1m", "rangeInSeconds": 60 },
-            { "range": "5m", "rangeInSeconds": 5 * 60 },
-            { "range": "10m", "rangeInSeconds": 10 * 60 },
-            { "range": "15m", "rangeInSeconds": 15 * 60 },
-            { "range": "30m", "rangeInSeconds": 30 * 60 },
-            { "range": "1h", "rangeInSeconds": 60 * 60 }
-        ];
-
-
-        vm.timeInterval = [1, 5, 10, 15, 30, 60];
-
-        vm.quickInsertData = {
-            timeStamp: _.now(),
-            id: "",
-            jsonPayload: "",
-            value: ""
-        };
-
-        vm.multiInsertData = {
-            id: "",
-            jsonPayload: ""
-        };
-        vm.rangeDurations = [1, 2, 5, 7];
-
-        vm.rangeInsertData = {
-            timeStamp: _.now(),
-            id: "",
-            selectedTimeInterval: 5,
-            jsonPayload: "",
-            startNumber: 1,
-            endNumber: 100,
-            selectedIntervalInMinutes: vm.timeInterval[2],
-            selectedDuration: vm.rangeDurations[1]
-        };
+    vm.streamingTimeRanges = [
+        { "range": "1s", "rangeInSeconds": 1 },
+        { "range": "5s", "rangeInSeconds": 5 },
+        { "range": "30s", "rangeInSeconds": 30 },
+        { "range": "1m", "rangeInSeconds": 60 },
+        { "range": "5m", "rangeInSeconds": 5 * 60 },
+        { "range": "10m", "rangeInSeconds": 10 * 60 },
+        { "range": "15m", "rangeInSeconds": 15 * 60 },
+        { "range": "30m", "rangeInSeconds": 30 * 60 },
+        { "range": "1h", "rangeInSeconds": 60 * 60 }
+    ];
 
 
-        vm.streamingInsertData = {
-            timeStamp: _.now(),
-            id: "",
-            jsonPayload: "",
-            count: 1,
-            startNumber: 1,
-            endNumber: 100,
-            isStreamingStarted: false,
-            lastStreamedValue: 2,
-            selectedRefreshInterval: vm.streamingTimeRanges[1].range
-        };
+    vm.timeInterval = [1, 5, 10, 15, 30, 60];
+
+    vm.quickInsertData = {
+        timeStamp: _.now(),
+        id: "",
+        jsonPayload: "",
+        value: ""
+    };
+
+    vm.multiInsertData = {
+        id: "",
+        jsonPayload: ""
+    };
+    vm.rangeDurations = [1, 2, 5, 7];
+
+    vm.rangeInsertData = {
+        timeStamp: _.now(),
+        id: "",
+        selectedTimeInterval: 5,
+        jsonPayload: "",
+        startNumber: 1,
+        endNumber: 100,
+        selectedIntervalInMinutes: vm.timeInterval[2],
+        selectedDuration: vm.rangeDurations[1]
+    };
 
 
-        vm.quickInsert = function (numberOfHoursPast) {
-            var computedTimestamp;
-
-            if (angular.isUndefined(numberOfHoursPast)) {
-                computedTimestamp = moment();
-            } else {
-                computedTimestamp = moment().subtract('hours', numberOfHoursPast);
-            }
-            $log.debug("Generated Timestamp is: " + computedTimestamp.fromNow());
-
-            vm.quickInsertData.jsonPayload = { timestamp: computedTimestamp.valueOf(), value: vm.quickInsertData.value };
-            $log.info("quick insert for id:  %s ", vm.quickInsertData.id);
-
-            metricDataService.insertSinglePayload(vm.quickInsertData.id, vm.quickInsertData.jsonPayload);
-
-            vm.quickInsertData.value = "";
-
-        };
+    vm.streamingInsertData = {
+        timeStamp: _.now(),
+        id: "",
+        jsonPayload: "",
+        count: 1,
+        startNumber: 1,
+        endNumber: 100,
+        isStreamingStarted: false,
+        lastStreamedValue: 2,
+        selectedRefreshInterval: vm.streamingTimeRanges[1].range
+    };
 
 
-        vm.multiInsert = function () {
-            metricDataService.insertMultiplePayload(vm.multiInsertData.jsonPayload);
-            vm.multiInsertData.jsonPayload = "";
-        };
+    vm.quickInsert = function (numberOfHoursPast) {
+        var computedTimestamp;
 
-
-        vm.rangeInsert = function () {
-            var jsonPayload;
-
-            jsonPayload = calculateRangeTimestamps(vm.rangeInsertData.id, vm.rangeInsertData.selectedDuration,
-                vm.rangeInsertData.selectedIntervalInMinutes, vm.rangeInsertData.startNumber,
-                vm.rangeInsertData.endNumber);
-            $log.debug("JsonPayload: " + jsonPayload);
-            metricDataService.insertMultiplePayload(jsonPayload);
-            vm.rangeInsertData.id = "";
-
-        };
-
-        function calculateRangeTimestamps(id, numberOfDays, intervalInMinutes, randomStart, randomEnd) {
-            var intervalTimestamps = [],
-                startDate = moment().subtract('days', numberOfDays).valueOf(),
-                endDate = _.now(),
-                step = intervalInMinutes * 60 * 1000,
-                startSeed = _.random(randomStart, randomEnd),
-                dbData = [];
-
-            intervalTimestamps = _.range(startDate, endDate, step);
-            dbData = _.map(intervalTimestamps, function(ts){
-                return {id: id, timestamp: ts, value: startSeed + _.random(-5, 5)};
-            });
-
-            return angular.toJson(dbData);
-
+        if (angular.isUndefined(numberOfHoursPast)) {
+            computedTimestamp = moment();
+        } else {
+            computedTimestamp = moment().subtract('hours', numberOfHoursPast);
         }
+        $log.debug("Generated Timestamp is: " + computedTimestamp.fromNow());
 
-        vm.startStreaming = function () {
-            var selectedTimeRangeInSeconds = 5;
+        vm.quickInsertData.jsonPayload = { timestamp: computedTimestamp.valueOf(), value: vm.quickInsertData.value };
+        $log.info("quick insert for id:  %s ", vm.quickInsertData.id);
 
-            angular.forEach(vm.streamingTimeRanges, function(value){
-              if(value.range === vm.streamingInsertData.selectedRefreshInterval)  {
-                 selectedTimeRangeInSeconds = value.rangeInSeconds;
-              }
-            });
-            vm.streamingInsertData.isStreamingStarted = true;
-            vm.streamingInsertData.count = 0;
-            vm.streamingInsertData.lastStreamedValue = 0;
-            streamingIntervalPromise = $interval(function () {
-                $log.log("Timer has Run! for seconds: " + selectedTimeRangeInSeconds);
-                vm.streamingInsertData.count = vm.streamingInsertData.count + 1;
-                vm.streamingInsertData.lastStreamedValue = _.random(vm.streamingInsertData.startNumber, vm.streamingInsertData.endNumber);
-                vm.streamingInsertData.jsonPayload = { timestamp: _.now(), value: vm.streamingInsertData.lastStreamedValue };
+        metricDataService.insertSinglePayload(vm.quickInsertData.id, vm.quickInsertData.jsonPayload);
 
-                metricDataService.insertSinglePayload(vm.streamingInsertData.id, vm.streamingInsertData.jsonPayload);
+        vm.quickInsertData.value = "";
 
-            }, selectedTimeRangeInSeconds * 1000);
-            $scope.$on('$destroy', function () {
-                $log.debug('Destroying intervalPromise');
-                $interval.cancel(streamingIntervalPromise);
-            });
+    };
 
-        };
 
-        vm.stopStreaming = function () {
-            toastr.info('Stop Streaming Data.');
-            $log.info('Stop Streaming Data.');
-            vm.streamingInsertData.isStreamingStarted = false;
+    vm.multiInsert = function () {
+        metricDataService.insertMultiplePayload(vm.multiInsertData.jsonPayload);
+        vm.multiInsertData.jsonPayload = "";
+    };
+
+
+    vm.rangeInsert = function () {
+        var jsonPayload;
+
+        jsonPayload = calculateRangeTimestamps(vm.rangeInsertData.id, vm.rangeInsertData.selectedDuration,
+            vm.rangeInsertData.selectedIntervalInMinutes, vm.rangeInsertData.startNumber,
+            vm.rangeInsertData.endNumber);
+        $log.debug("JsonPayload: " + jsonPayload);
+        metricDataService.insertMultiplePayload(jsonPayload);
+        vm.rangeInsertData.id = "";
+
+    };
+
+    function calculateRangeTimestamps(id, numberOfDays, intervalInMinutes, randomStart, randomEnd) {
+        var intervalTimestamps = [],
+            startDate = moment().subtract('days', numberOfDays).valueOf(),
+            endDate = _.now(),
+            step = intervalInMinutes * 60 * 1000,
+            startSeed = _.random(randomStart, randomEnd),
+            dbData = [];
+
+        intervalTimestamps = _.range(startDate, endDate, step);
+        dbData = _.map(intervalTimestamps, function(ts){
+            return {id: id, timestamp: ts, value: startSeed + _.random(-5, 5)};
+        });
+
+        return angular.toJson(dbData);
+
+    }
+
+    vm.startStreaming = function () {
+        var selectedTimeRangeInSeconds = 5;
+
+        angular.forEach(vm.streamingTimeRanges, function(value){
+            if(value.range === vm.streamingInsertData.selectedRefreshInterval)  {
+                selectedTimeRangeInSeconds = value.rangeInSeconds;
+            }
+        });
+        vm.streamingInsertData.isStreamingStarted = true;
+        vm.streamingInsertData.count = 0;
+        vm.streamingInsertData.lastStreamedValue = 0;
+        streamingIntervalPromise = $interval(function () {
+            $log.log("Timer has Run! for seconds: " + selectedTimeRangeInSeconds);
+            vm.streamingInsertData.count = vm.streamingInsertData.count + 1;
+            vm.streamingInsertData.lastStreamedValue = _.random(vm.streamingInsertData.startNumber, vm.streamingInsertData.endNumber);
+            vm.streamingInsertData.jsonPayload = { timestamp: _.now(), value: vm.streamingInsertData.lastStreamedValue };
+
+            metricDataService.insertSinglePayload(vm.streamingInsertData.id, vm.streamingInsertData.jsonPayload);
+
+        }, selectedTimeRangeInSeconds * 1000);
+        $scope.$on('$destroy', function () {
+            $log.debug('Destroying intervalPromise');
             $interval.cancel(streamingIntervalPromise);
-        };
+        });
+
+    };
+
+    vm.stopStreaming = function () {
+        toastr.info('Stop Streaming Data.');
+        $log.info('Stop Streaming Data.');
+        vm.streamingInsertData.isStreamingStarted = false;
+        $interval.cancel(streamingIntervalPromise);
+    };
 
 
-    }]);
+}
+angular.module('chartingApp')
+    .controller('InsertMetricsController', ['$scope', '$rootScope', '$log','$interval', 'metricDataService', InsertMetricsController ]);
