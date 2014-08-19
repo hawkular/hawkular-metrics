882c4d52b1c1ac9b1908eb6983d23c767fc1b785
Mike Thompson <mithomps@redhat.com>
2014-08-19 11:31:23 -0700

    Replace charting services with promises.

--- console/src/main/webapp/scripts/services/metric-data-service.js
+++ console/src/main/webapp/scripts/services/metric-data-service.js
@@ -1,36 +1,43 @@
 'use strict';
 
 angular.module('rhqm.services')
-    .factory('metricDataService', ['$q', '$rootScope','$http', '$log', '$localStorage', 'BASE_URL', function ($q, $rootScope, $http, $log, $localStorage, BASE_URL) {
+    .factory('metricDataService', ['$q', '$rootScope', '$http', '$log', '$localStorage', 'BASE_URL', function ($q, $rootScope, $http, $log, $localStorage, BASE_URL) {
 
-        function makeBaseUrl () {
-            var baseUrl = 'http://' + $rootScope.$storage.server + ':' + $rootScope.$storage.port +  BASE_URL;
-            $log.debug('MakeUrl: '+ baseUrl);
+        function makeBaseUrl() {
+            var baseUrl = 'http://' + $rootScope.$storage.server + ':' + $rootScope.$storage.port + BASE_URL;
+            $log.debug('MakeUrl: ' + baseUrl);
             return baseUrl;
         }
 
         return {
             getMetricsForTimeRange: function (id, startDate, endDate, buckets) {
-                $log.info("Retrieving metrics data for id: " + id);
-                $log.info("Date Range: " + new Date(startDate) + " - " + new Date(endDate));
+                $log.info("-- Retrieving metrics data for id: " + id);
+                $log.info("-- Date Range: " + new Date(startDate) + " - " + new Date(endDate));
                 var numBuckets = buckets || 60,
                     base = makeBaseUrl(),
-                    promise = $q.defer();
-
-                if (startDate >= endDate) {
-                    $log.warn("Start date was after end date");
-                    return;
-                }
-
-                return $http.get(base + '/' + id,
+                    deferred = $q.defer(),
+                    searchParams =
                     {
                         params: {
                             start: startDate.getTime(),
                             end: endDate.getTime(),
                             buckets: numBuckets
                         }
-                    }
-                );
+                    };
+
+                if (startDate >= endDate) {
+                    $log.warn("Start date was after end date");
+                    deferred.reject("Start date was after end date");
+                }
+
+                $http.get(base + '/' + id, searchParams).success(function (data) {
+                    deferred.resolve(data);
+                }).error(function (reason, status) {
+                    $log.error('Error Loading Chart Data:' + status + ", " + reason);
+                    deferred.reject(status + " - " + reason);
+                });
+
+                return deferred.promise;
             },
 
             insertSinglePayload: function (id, jsonPayload) {
