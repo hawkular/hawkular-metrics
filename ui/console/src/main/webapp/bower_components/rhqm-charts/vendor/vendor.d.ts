6550f492bc38b90ca7cbb05c096b891744702d13
Juraci Paixão Kröhling <juraci@kroehling.de>
2014-10-31 14:12:07 +0100

    Added bower_components to gitignore and removed bower_components.

--- ui/console/src/main/webapp/bower_components/rhqm-charts/vendor/vendor.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="angular/angular.d.ts" />
-/// <reference path="angular/angular-ui-router.d.ts" />
-/// <reference path="angular/angular-ui-bootstrap.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
-/// <reference path="moment/moment.d.ts" />
-/// <reference path="toastr/toastr.d.ts" />
