19bd061bd290dc8d1f32485f03b5ff0360233cb4
Thomas Segismont <tsegismo@redhat.com>
2014-11-10 22:24:18 +0100

    Create ptrans fat jar
    -> Makes it easier to consume ptrans (once it will be released): java -jar ptrans-VERSION-jar-with-dependencies.jar
    -> Makes it easier to add deps without looking at start.sh
    
    Also, added Maven exec plugin config, so that a simple "mvn exec:java" starts the program

--- clients/ptranslator/start.sh
+++ clients/ptranslator/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-
+echo "Checking project version..."
 VERSION=`mvn  help:evaluate -Dexpression=project.version | grep -vF "[INFO]"`
-MVN_REPO=`mvn help:evaluate -Dexpression=settings.localRepository | grep -vF "[INFO]"`
+echo "Project version is ${VERSION}"
 
 echo "(Re)building the commons library"
 cd ../common
@@ -12,12 +12,4 @@ cd -
 echo "(Re)building ptrans"
 mvn install
 
-java -Djava.net.preferIPv4Stack=true \
-   -cp ${MVN_REPO}/io/netty/netty-all/4.0.24.Final/netty-all-4.0.24.Final.jar\
-:target/ptrans-$VERSION.jar\
-:../common/target/clients-common-$VERSION.jar\
-:${HOME}/.m2/repository/org/slf4j/slf4j-log4j12/1.7.7/slf4j-log4j12-1.7.7.jar\
-:${HOME}/.m2/repository/org/slf4j/slf4j-api/1.7.7/slf4j-api-1.7.7.jar\
-:${HOME}/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar\
-:${HOME}/.m2/repository/org/acplt/oncrpc/1.0.7/oncrpc-1.0.7.jar\
- org.rhq.metrics.clients.ptrans.Main $*
+java -Djava.net.preferIPv4Stack=true -jar target/ptrans-${VERSION}-jar-with-dependencies.jar $*
