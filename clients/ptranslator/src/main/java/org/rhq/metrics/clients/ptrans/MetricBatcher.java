b30bf59526774669f24d3a22a6c1da3edc1a5ae3
Heiko W. Rupp <hwr@pilhuhn.de>
2014-11-18 22:01:25 +0100

    Make the individual batch sizes configurable.
    Could be extended to size setting per input source and
    for some time based batching (e.g. batch up to a minute)

--- clients/ptranslator/src/main/java/org/rhq/metrics/clients/ptrans/MetricBatcher.java
+++ clients/ptranslator/src/main/java/org/rhq/metrics/clients/ptrans/MetricBatcher.java
@@ -10,36 +10,52 @@ import io.netty.util.AttributeKey;
 import org.rhq.metrics.client.common.SingleMetric;
 
 /**
- * Batch several individual {@link org.rhq.metrics.client.common.SingleMetric} objects in a batch of {@link MetricBatcher#THRESHOLD}
+ * Batch several individual {@link org.rhq.metrics.client.common.SingleMetric} objects in a batch of {@link MetricBatcher#minimumBatchSize}
  * items to reduce backend communication overhead.
- *
- * TODO how can we detect that Netty is going down and first send the last batch?
  *
  * @author Heiko W. Rupp
  */
 public class MetricBatcher extends MessageToMessageDecoder<SingleMetric> {
 
-    private static final int THRESHOLD = 5;
-    private String subKey;
+    private int minimumBatchSize;
 
-    public MetricBatcher(String subKey) {
-        this.subKey = subKey;
-        cacheKey = AttributeKey.valueOf("cachedMetrics"+subKey);
+    /**
+     * Create a batcher with the passed batch size
+     * @param subKey Identification of the metrics of this batcher
+     * @param minimumBatchSize Size of batches. If the number is less than 1, then 1 is used.
+     */
+    public MetricBatcher(String subKey, int minimumBatchSize) {
+        this.minimumBatchSize = minimumBatchSize;
+        if (this.minimumBatchSize<1) {
+            this.minimumBatchSize=1;
+        }
+        cacheKey = AttributeKey.valueOf("cachedMetrics."+subKey);
     }
 
     AttributeKey<List<SingleMetric>> cacheKey;
 
+    /**
+     * Batch up incoming SingleMetric messages. If the #minimumBatchSize is not yet reached, the messages are stored
+     * locally. Otherwise the list of messages will be forwarded to the next handler.
+     * This method will be called for each written message that can be handled
+     * by this encoder.
+     *
+     * @param ctx           the {@link ChannelHandlerContext} which this {@link MessageToMessageDecoder} belongs to
+     * @param msg           the SingleMetric to be batched up
+     * @param out           the {@link List} to which decoded messages should be added if the batch size is reached
+     * @throws Exception    is thrown if an error occurs
+     */
     @Override
     protected void decode(ChannelHandlerContext ctx, SingleMetric msg, List<Object> out) throws Exception {
 
         List<SingleMetric> cached = ctx.attr(cacheKey).get();
         if (cached==null) {
-            cached = new ArrayList<>(5);
+            cached = new ArrayList<>(minimumBatchSize);
             ctx.attr(cacheKey).set(cached);
         }
 
-        if (cached.size()  >= THRESHOLD) {
-            List<SingleMetric> toForward = new ArrayList<>(6);
+        if (cached.size()  >= minimumBatchSize) {
+            List<SingleMetric> toForward = new ArrayList<>(cached.size()+1);
             toForward.addAll(cached);
             toForward.add(msg);
             cached.clear();
