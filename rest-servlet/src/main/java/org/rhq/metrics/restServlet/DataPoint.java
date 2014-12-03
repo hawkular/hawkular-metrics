hawkular/hawkular-metrics

commit 65be19275aef40321e3d864e4d039f6e34264b94

removing some unnecessary classes
Doing a bit of clean up and removing/combining some classes used for json
responses. There is more, similar clean up that can be done. I will tackle it
in a subsequent commit. More test coverage is needed first though.

rest-servlet/src/main/java/org/rhq/metrics/restServlet/DataPoint.java
@@ -1,7 +1,11 @@
 package org.rhq.metrics.restServlet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.wordnik.swagger.annotations.ApiClass;
 import com.wordnik.swagger.annotations.ApiProperty;
 
@@ -14,14 +18,23 @@ import com.wordnik.swagger.annotations.ApiProperty;
 public class DataPoint {
 
     private long timestamp;
-    private double value;
+    private Object value;
+
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+    private Set<String> tags = new HashSet<>();
 
     public DataPoint() {
     }
 
-    public DataPoint(long timestamp, double value) {
+    public DataPoint(long timestamp, Object value) {
         this.timestamp = timestamp;
         this.value = value;
+    }
+
+    public DataPoint(long timestamp, Object value, Set<String> tags) {
+        this.timestamp = timestamp;
+        this.value = value;
+        this.tags = tags;
     }
 
     @ApiProperty("Time when the value was obtained in milliseconds since epoch")
@@ -34,11 +47,19 @@ public class DataPoint {
     }
 
     @ApiProperty("The value of this data point")
-    public double getValue() {
+    public Object getValue() {
         return value;
     }
 
-    public void setValue(double value) {
+    public void setValue(Object value) {
         this.value = value;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
     }
 }

rest-servlet/src/main/java/org/rhq/metrics/restServlet/MetricHandler.java
@@ -314,7 +314,7 @@ public class MetricHandler {
                     metric = new NumericMetric2(DEFAULT_TENANT_ID, new MetricId(p.getId()));
                 }
             }
-            metric.addData(p.getTimestamp(), p.getValue());
+            metric.addData(p.getTimestamp(), (double) p.getValue());
         }
         metrics.add(metric);
 
@@ -418,20 +418,15 @@ public class MetricHandler {
         Futures.addCallback(queryFuture, new FutureCallback<Map<MetricId, Set<NumericData>>>() {
             @Override
             public void onSuccess(Map<MetricId, Set<NumericData>> taggedDataMap) {
-                Map<String, NumericDataOut> results = new HashMap<>();
-                NumericDataOut dataOut = null;
+                Map<String, MetricOut> results = new HashMap<>();
+                MetricOut dataOut = null;
                 for (MetricId id : taggedDataMap.keySet()) {
-                    List<NumericDataPoint> dataPoints = new ArrayList<>();
+                    List<DataPoint> dataPoints = new ArrayList<>();
                     for (NumericData d : taggedDataMap.get(id)) {
                         if (dataOut == null) {
-                            dataOut = new NumericDataOut();
-                            dataOut.setTenantId(d.getMetric().getTenantId());
-                            dataOut.setName(d.getMetric().getId().getName());
+                            dataOut = new MetricOut(d.getMetric().getTenantId(), d.getMetric().getId().getName(), null);
                         }
-                        NumericDataPoint p = new NumericDataPoint();
-                        p.setTimestamp(d.getTimestamp());
-                        p.setValue(d.getValue());
-                        dataPoints.add(p);
+                        dataPoints.add(new DataPoint(d.getTimestamp(), d.getValue()));
                     }
                     dataOut.setData(dataPoints);
                     results.put(id.getName(), dataOut);
@@ -460,20 +455,16 @@ public class MetricHandler {
                 if (taggedDataMap.isEmpty()) {
                     asyncResponse.resume(Response.ok().status(Response.Status.NO_CONTENT).build());
                 } else {
-                    Map<String, AvailabilityDataOut> results = new HashMap<>();
-                    AvailabilityDataOut dataOut = null;
+                    Map<String, MetricOut> results = new HashMap<>();
+                    MetricOut dataOut = null;
                     for (MetricId id : taggedDataMap.keySet()) {
-                        List<AvailabilityDataPoint> dataPoints = new ArrayList<>();
+                        List<DataPoint> dataPoints = new ArrayList<>();
                         for (Availability a : taggedDataMap.get(id)) {
                             if (dataOut == null) {
-                                dataOut = new AvailabilityDataOut();
-                                dataOut.setTenantId(a.getMetric().getTenantId());
-                                dataOut.setName(a.getMetric().getId().getName());
+                                dataOut = new MetricOut(a.getMetric().getTenantId(), a.getMetric().getId().getName(),
+                                    null);
                             }
-                            AvailabilityDataPoint p = new AvailabilityDataPoint();
-                            p.setTimestamp(a.getTimestamp());
-                            p.setValue(a.getType().getText());
-                            dataPoints.add(p);
+                            dataPoints.add(new DataPoint(a.getTimestamp(), a.getType().getText()));
                         }
                         dataOut.setData(dataPoints);
                         results.put(id.getName(), dataOut);
@@ -523,18 +514,14 @@ public class MetricHandler {
                 if (metric == null) {
                     asyncResponse.resume(Response.ok().status(Response.Status.NO_CONTENT).build());
                 } else {
-                    NumericDataOut output = new NumericDataOut();
-                    output.setTenantId(metric.getTenantId());
-                    output.setName(metric.getId().getName());
-                    output.setMetadata(metric.getMetadata());
-
-                    List<NumericDataPoint> dataPoints = new ArrayList<>(metric.getData().size());
+                    MetricOut output = new MetricOut(metric.getTenantId(), metric.getId().getName(),
+                        metric.getMetadata());
+                    List<DataPoint> dataPoints = new ArrayList<>();
                     for (NumericData d : metric.getData()) {
-                        NumericDataPoint dataPoint = new NumericDataPoint(d.getTimestamp(), d.getValue());
-                        dataPoint.setTags(getTagNames(d));
-                       dataPoints.add(dataPoint);
+                        dataPoints.add(new DataPoint(d.getTimestamp(), d.getValue(), getTagNames(d)));
                     }
                     output.setData(dataPoints);
+
                     asyncResponse.resume(Response.ok(output).type(MediaType.APPLICATION_JSON_TYPE).build());
                 }
             }
@@ -568,18 +555,11 @@ public class MetricHandler {
                 if (metric == null) {
                     asyncResponse.resume(Response.ok().status(Response.Status.NO_CONTENT).build());
                 } else {
-                    AvailabilityDataOut output = new AvailabilityDataOut();
-                    output.setTenantId(metric.getTenantId());
-                    output.setName(metric.getId().getName());
-                    output.setMetadata(metric.getMetadata());
-
-                    List<AvailabilityDataPoint> dataPoints = new ArrayList<>(metric.getData().size());
+                    MetricOut output = new MetricOut(metric.getTenantId(), metric.getId().getName(),
+                        metric.getMetadata());
+                    List<DataPoint> dataPoints = new ArrayList<>(metric.getData().size());
                     for (Availability a : metric.getData()) {
-                        AvailabilityDataPoint p = new AvailabilityDataPoint();
-                        p.setTimestamp(a.getTimestamp());
-                        p.setValue(a.getType().getText());
-                        p.setTags(getTagNames(a));
-                        dataPoints.add(p);
+                        dataPoints.add(new DataPoint(a.getTimestamp(), a.getType().getText(), getTagNames(a)));
                     }
                     output.setData(dataPoints);
 
@@ -660,20 +640,16 @@ public class MetricHandler {
                     // will always have a null metadata field, which might misleading. We may
                     // want to use a different return type that does not have a meta data property.
 
-                    Map<String, NumericDataOut> results = new HashMap<>();
-                    NumericDataOut dataOut = null;
+                    Map<String, MetricOut> results = new HashMap<>();
+                    MetricOut dataOut = null;
                     for (MetricId id : taggedDataMap.keySet()) {
-                        List<NumericDataPoint> dataPoints = new ArrayList<>();
+                        List<DataPoint> dataPoints = new ArrayList<>();
                         for (NumericData d : taggedDataMap.get(id)) {
                             if (dataOut == null) {
-                                dataOut = new NumericDataOut();
-                                dataOut.setTenantId(d.getMetric().getTenantId());
-                                dataOut.setName(d.getMetric().getId().getName());
+                                dataOut = new MetricOut(d.getMetric().getTenantId(), d.getMetric().getId().getName(),
+                                    null);
                             }
-                            NumericDataPoint p = new NumericDataPoint();
-                            p.setTimestamp(d.getTimestamp());
-                            p.setValue(d.getValue());
-                            dataPoints.add(p);
+                            dataPoints.add(new DataPoint(d.getTimestamp(), d.getValue()));
                         }
                         dataOut.setData(dataPoints);
                         results.put(id.getName(), dataOut);
@@ -702,20 +678,16 @@ public class MetricHandler {
                 if (taggedDataMap.isEmpty()) {
                     asyncResponse.resume(Response.ok().status(Response.Status.NO_CONTENT).build());
                 } else {
-                    Map<String, AvailabilityDataOut> results = new HashMap<>();
-                    AvailabilityDataOut dataOut = null;
+                    Map<String, MetricOut> results = new HashMap<>();
+                    MetricOut dataOut = null;
                     for (MetricId id : taggedDataMap.keySet()) {
-                        List<AvailabilityDataPoint> dataPoints = new ArrayList<>();
+                        List<DataPoint> dataPoints = new ArrayList<>();
                         for (Availability a : taggedDataMap.get(id)) {
                             if (dataOut == null) {
-                                dataOut = new AvailabilityDataOut();
-                                dataOut.setTenantId(a.getMetric().getTenantId());
-                                dataOut.setName(a.getMetric().getId().getName());
+                                dataOut = new MetricOut(a.getMetric().getTenantId(), a.getMetric().getId().getName(),
+                                    null);
                             }
-                            AvailabilityDataPoint p = new AvailabilityDataPoint();
-                            p.setTimestamp(a.getTimestamp());
-                            p.setValue(a.getType().getText());
-                            dataPoints.add(p);
+                            dataPoints.add(new DataPoint(a.getTimestamp(), a.getType().getText()));
                         }
                         dataOut.setData(dataPoints);
                         results.put(id.getName(), dataOut);

rest-servlet/src/main/java/org/rhq/metrics/restServlet/MetricOut.java
@@ -1,6 +1,8 @@
 package org.rhq.metrics.restServlet;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -17,6 +19,9 @@ public class MetricOut {
 
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
     private Map<String, String> metadata = new HashMap<>();
+
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+    private List<DataPoint> data = new ArrayList<>();
 
     public MetricOut() {
     }
@@ -49,5 +54,13 @@ public class MetricOut {
 
     public void setMetadata(Map<String, String> metadata) {
         this.metadata = metadata;
+    }
+
+    public List<DataPoint> getData() {
+        return data;
+    }
+
+    public void setData(List<DataPoint> data) {
+        this.data = data;
     }
 }

rest-servlet/src/main/java/org/rhq/metrics/restServlet/AvailabilityDataOut.java
@@ -1,65 +1,0 @@
-package org.rhq.metrics.restServlet;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.google.common.base.Objects;
-
-/**
- * @author John Sanda
- */
-public class AvailabilityDataOut {
-
-    private String tenantId;
-
-    private String name;
-
-    private Map<String, String> metadata = new HashMap<>();
-
-    private List<AvailabilityDataPoint> data = new ArrayList<>();
-
-    public String getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(String tenantId) {
-        this.tenantId = tenantId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Map<String, String> getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(Map<String, String> metadata) {
-        this.metadata = metadata;
-    }
-
-    public List<AvailabilityDataPoint> getData() {
-        return data;
-    }
-
-    public void setData(List<AvailabilityDataPoint> data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return Objects.toStringHelper(this)
-            .add("tenantId", tenantId)
-            .add("name", name)
-            .add("metadata", metadata)
-            .add("data", data)
-            .toString();
-    }
-
-}

rest-servlet/src/main/java/org/rhq/metrics/restServlet/NumericDataOut.java
@@ -1,64 +1,0 @@
-package org.rhq.metrics.restServlet;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.google.common.base.Objects;
-
-/**
- * @author John Sanda
- */
-public class NumericDataOut {
-
-    private String tenantId;
-
-    private String name;
-
-    private Map<String, String> metadata = new HashMap<>();
-
-    private List<NumericDataPoint> data = new ArrayList<>();
-
-    public String getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(String tenantId) {
-        this.tenantId = tenantId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Map<String, String> getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(Map<String, String> metadata) {
-        this.metadata = metadata;
-    }
-
-    public List<NumericDataPoint> getData() {
-        return data;
-    }
-
-    public void setData(List<NumericDataPoint> data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return Objects.toStringHelper(this)
-            .add("tenantId", tenantId)
-            .add("name", name)
-            .add("metadata", metadata)
-            .add("data", data)
-            .toString();
-    }
-}

commit a38f194c718cfe16cc482cfc0ae3741d1f63caea

revert some of the changes from commit 65be1927
The refactoring in 65be1927 broke some of the old REST endpoints, and we want
to keep those funcitonal for now.

rest-servlet/src/main/java/org/rhq/metrics/restServlet/DataPoint.java
@@ -1,11 +1,7 @@
 package org.rhq.metrics.restServlet;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.xml.bind.annotation.XmlRootElement;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.wordnik.swagger.annotations.ApiClass;
 import com.wordnik.swagger.annotations.ApiProperty;
 
@@ -18,23 +14,14 @@ import com.wordnik.swagger.annotations.ApiProperty;
 public class DataPoint {
 
     private long timestamp;
-    private Object value;
-
-    @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
-    private Set<String> tags = new HashSet<>();
+    private double value;
 
     public DataPoint() {
     }
 
-    public DataPoint(long timestamp, Object value) {
+    public DataPoint(long timestamp, double value) {
         this.timestamp = timestamp;
         this.value = value;
-    }
-
-    public DataPoint(long timestamp, Object value, Set<String> tags) {
-        this.timestamp = timestamp;
-        this.value = value;
-        this.tags = tags;
     }
 
     @ApiProperty("Time when the value was obtained in milliseconds since epoch")
@@ -47,19 +34,11 @@ public class DataPoint {
     }
 
     @ApiProperty("The value of this data point")
-    public Object getValue() {
+    public double getValue() {
         return value;
     }
 
-    public void setValue(Object value) {
+    public void setValue(double value) {
         this.value = value;
-    }
-
-    public Set<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(Set<String> tags) {
-        this.tags = tags;
     }
 }

rest-servlet/src/main/java/org/rhq/metrics/restServlet/DataPointOut.java
@@ -1,0 +1,57 @@
+package org.rhq.metrics.restServlet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+/**
+ * @author John Sanda
+ */
+public class DataPointOut {
+
+    private long timestamp;
+    private Object value;
+
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+    private Set<String> tags = new HashSet<>();
+
+    public DataPointOut() {
+    }
+
+    public DataPointOut(long timestamp, Object value) {
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+
+    public DataPointOut(long timestamp, Object value, Set<String> tags) {
+        this.timestamp = timestamp;
+        this.value = value;
+        this.tags = tags;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+
+}

rest-servlet/src/main/java/org/rhq/metrics/restServlet/MetricHandler.java
@@ -314,7 +314,7 @@ public class MetricHandler {
                     metric = new NumericMetric2(DEFAULT_TENANT_ID, new MetricId(p.getId()));
                 }
             }
-            metric.addData(p.getTimestamp(), (double) p.getValue());
+            metric.addData(p.getTimestamp(), (Double) p.getValue());
         }
         metrics.add(metric);
 
@@ -421,12 +421,12 @@ public class MetricHandler {
                 Map<String, MetricOut> results = new HashMap<>();
                 MetricOut dataOut = null;
                 for (MetricId id : taggedDataMap.keySet()) {
-                    List<DataPoint> dataPoints = new ArrayList<>();
+                    List<DataPointOut> dataPoints = new ArrayList<>();
                     for (NumericData d : taggedDataMap.get(id)) {
                         if (dataOut == null) {
                             dataOut = new MetricOut(d.getMetric().getTenantId(), d.getMetric().getId().getName(), null);
                         }
-                        dataPoints.add(new DataPoint(d.getTimestamp(), d.getValue()));
+                        dataPoints.add(new DataPointOut(d.getTimestamp(), d.getValue()));
                     }
                     dataOut.setData(dataPoints);
                     results.put(id.getName(), dataOut);
@@ -458,13 +458,13 @@ public class MetricHandler {
                     Map<String, MetricOut> results = new HashMap<>();
                     MetricOut dataOut = null;
                     for (MetricId id : taggedDataMap.keySet()) {
-                        List<DataPoint> dataPoints = new ArrayList<>();
+                        List<DataPointOut> dataPoints = new ArrayList<>();
                         for (Availability a : taggedDataMap.get(id)) {
                             if (dataOut == null) {
                                 dataOut = new MetricOut(a.getMetric().getTenantId(), a.getMetric().getId().getName(),
                                     null);
                             }
-                            dataPoints.add(new DataPoint(a.getTimestamp(), a.getType().getText()));
+                            dataPoints.add(new DataPointOut(a.getTimestamp(), a.getType().getText()));
                         }
                         dataOut.setData(dataPoints);
                         results.put(id.getName(), dataOut);
@@ -516,9 +516,9 @@ public class MetricHandler {
                 } else {
                     MetricOut output = new MetricOut(metric.getTenantId(), metric.getId().getName(),
                         metric.getMetadata());
-                    List<DataPoint> dataPoints = new ArrayList<>();
+                    List<DataPointOut> dataPoints = new ArrayList<>();
                     for (NumericData d : metric.getData()) {
-                        dataPoints.add(new DataPoint(d.getTimestamp(), d.getValue(), getTagNames(d)));
+                        dataPoints.add(new DataPointOut(d.getTimestamp(), d.getValue(), getTagNames(d)));
                     }
                     output.setData(dataPoints);
 
@@ -557,9 +557,9 @@ public class MetricHandler {
                 } else {
                     MetricOut output = new MetricOut(metric.getTenantId(), metric.getId().getName(),
                         metric.getMetadata());
-                    List<DataPoint> dataPoints = new ArrayList<>(metric.getData().size());
+                    List<DataPointOut> dataPoints = new ArrayList<>(metric.getData().size());
                     for (Availability a : metric.getData()) {
-                        dataPoints.add(new DataPoint(a.getTimestamp(), a.getType().getText(), getTagNames(a)));
+                        dataPoints.add(new DataPointOut(a.getTimestamp(), a.getType().getText(), getTagNames(a)));
                     }
                     output.setData(dataPoints);
 
@@ -643,13 +643,13 @@ public class MetricHandler {
                     Map<String, MetricOut> results = new HashMap<>();
                     MetricOut dataOut = null;
                     for (MetricId id : taggedDataMap.keySet()) {
-                        List<DataPoint> dataPoints = new ArrayList<>();
+                        List<DataPointOut> dataPoints = new ArrayList<>();
                         for (NumericData d : taggedDataMap.get(id)) {
                             if (dataOut == null) {
                                 dataOut = new MetricOut(d.getMetric().getTenantId(), d.getMetric().getId().getName(),
                                     null);
                             }
-                            dataPoints.add(new DataPoint(d.getTimestamp(), d.getValue()));
+                            dataPoints.add(new DataPointOut(d.getTimestamp(), d.getValue()));
                         }
                         dataOut.setData(dataPoints);
                         results.put(id.getName(), dataOut);
@@ -681,13 +681,13 @@ public class MetricHandler {
                     Map<String, MetricOut> results = new HashMap<>();
                     MetricOut dataOut = null;
                     for (MetricId id : taggedDataMap.keySet()) {
-                        List<DataPoint> dataPoints = new ArrayList<>();
+                        List<DataPointOut> dataPoints = new ArrayList<>();
                         for (Availability a : taggedDataMap.get(id)) {
                             if (dataOut == null) {
                                 dataOut = new MetricOut(a.getMetric().getTenantId(), a.getMetric().getId().getName(),
                                     null);
                             }
-                            dataPoints.add(new DataPoint(a.getTimestamp(), a.getType().getText()));
+                            dataPoints.add(new DataPointOut(a.getTimestamp(), a.getType().getText()));
                         }
                         dataOut.setData(dataPoints);
                         results.put(id.getName(), dataOut);

rest-servlet/src/main/java/org/rhq/metrics/restServlet/MetricOut.java
@@ -21,7 +21,7 @@ public class MetricOut {
     private Map<String, String> metadata = new HashMap<>();
 
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
-    private List<DataPoint> data = new ArrayList<>();
+    private List<DataPointOut> data = new ArrayList<>();
 
     public MetricOut() {
     }
@@ -56,11 +56,11 @@ public class MetricOut {
         this.metadata = metadata;
     }
 
-    public List<DataPoint> getData() {
+    public List<DataPointOut> getData() {
         return data;
     }
 
-    public void setData(List<DataPoint> data) {
+    public void setData(List<DataPointOut> data) {
         this.data = data;
     }
 }