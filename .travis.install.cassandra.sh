891541b30768a8db1827f89144c17cc7a357f061
Thomas Segismont <tsegismo@redhat.com>
2014-12-05 14:29:00 +0100

    No ulimit??

--- .travis.install.cassandra.sh
+++ .travis.install.cassandra.sh
@@ -10,7 +10,7 @@ tar -xzf dsc-cassandra-2.1.1-bin.tar.gz
 
 mkdir dsc-cassandra-2.1.1/logs
 
-sudo ulimit -c
+sudo bash -c 'ulimit -c'
 
 sudo bash -c 'ulimit -c unlimited; HEAP_NEWSIZE="100M";MAX_HEAP_SIZE="1G"; sh dsc-cassandra-2.1.1/bin/cassandra -f -p ${HOME}/cassandra.pid > dsc-cassandra-2.1.1/logs/stdout.log 2>&1'
 
