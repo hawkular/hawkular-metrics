ffda216ce415148ac0d924302f2525468ea6cb15
Thomas Segismont <tsegismo@redhat.com>
2014-12-05 14:31:58 +0100

    Start with -f but then go to the background

--- .travis.install.cassandra.sh
+++ .travis.install.cassandra.sh
@@ -12,5 +12,5 @@ mkdir dsc-cassandra-2.1.1/logs
 
 sudo bash -c 'ulimit -c'
 
-sudo bash -c 'ulimit -c unlimited; HEAP_NEWSIZE="100M";MAX_HEAP_SIZE="1G"; sh dsc-cassandra-2.1.1/bin/cassandra -f -p ${HOME}/cassandra.pid > dsc-cassandra-2.1.1/logs/stdout.log 2>&1'
+sudo bash -c 'ulimit -c unlimited; HEAP_NEWSIZE="100M";MAX_HEAP_SIZE="1G"; nohup sh dsc-cassandra-2.1.1/bin/cassandra -f -p ${HOME}/cassandra.pid > dsc-cassandra-2.1.1/logs/stdout.log 2>&1 &'
 
