27b6c1b884ed8f3512455bd8511901513dc6ed3d
Thomas Segismont <tsegismo@redhat.com>
2014-12-05 14:50:29 +0100

    Where does Travis store core dumps?

--- .travis.install.cassandra.sh
+++ .travis.install.cassandra.sh
@@ -10,6 +10,8 @@ tar -xzf dsc-cassandra-2.1.1-bin.tar.gz
 
 mkdir dsc-cassandra-2.1.1/logs
 
+cat /proc/sys/kernel/core_pattern
+
 sudo bash -c 'ulimit -c'
 
 sudo bash -c 'ulimit -c unlimited; HEAP_NEWSIZE="100M";MAX_HEAP_SIZE="1G"; nohup sh dsc-cassandra-2.1.1/bin/cassandra -f -p ${HOME}/cassandra.pid > dsc-cassandra-2.1.1/logs/stdout.log 2>&1 &'
