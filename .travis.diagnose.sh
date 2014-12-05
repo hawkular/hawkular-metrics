4c717c2bb7b1a9709531ffe608b947451f9d250f
Thomas Segismont <tsegismo@redhat.com>
2014-12-05 11:51:59 +0100

    'nodetool status' blocks when C* is down

--- .travis.diagnose.sh
+++ .travis.diagnose.sh
@@ -6,11 +6,9 @@ nc -z -w5 127.0.0.1 9042; echo $?
 
 CASSANDRA_PID=`cat ${HOME}/cassandra.pid`
 
-ps -f -p $CASSANDRA_PID
+ps -f -p $CASSANDRA_PID && cat `dsc-cassandra-2.1.1/bin/nodetool status`
 
 ps aux | grep java
-
-cat `dsc-cassandra-2.1.1/bin/nodetool status`
 
 cat dsc-cassandra-2.1.1/logs/stderr.log
 
