be7cff52f6472b8e1e4ef72dfcf50a5804b4e3da
Thomas Segismont <tsegismo@redhat.com>
2014-12-12 16:47:41 +0100

    Added CustomMediaTypes

---
+++ rest-servlet/src/main/java/org/rhq/metrics/restServlet/CustomMediaTypes.java
@@ -0,0 +1,13 @@
+package org.rhq.metrics.restServlet;
+
+/**
+ * @author Thomas Segismont
+ */
+public class CustomMediaTypes {
+
+    public static final String APPLICATION_VND_RHQ_WRAPPED_JSON = "application/vnd.rhq.wrapped+json";
+
+    private CustomMediaTypes() {
+        // Constants class
+    }
+}
