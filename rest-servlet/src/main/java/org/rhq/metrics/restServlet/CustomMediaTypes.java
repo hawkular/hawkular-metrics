46920f3709804d13aa9c16e9b1e011f318790a34
Thomas Segismont <tsegismo@redhat.com>
2014-12-16 18:51:26 +0100

    JSONP: convert entities when media type is 'application/javascript'
    Before the change, the JsonPFilter allowed JSONP for any resource, regardless of the media type (it could even wrap XML).
    
    Now JSONP needs to be explicitely allowed (with @Produces)
    
    Being a MessageBodyWriter JsonPProvider lets the JAX-RS runtime determine the content length.

--- rest-servlet/src/main/java/org/rhq/metrics/restServlet/CustomMediaTypes.java
+++ rest-servlet/src/main/java/org/rhq/metrics/restServlet/CustomMediaTypes.java
@@ -17,11 +17,19 @@
 package org.rhq.metrics.restServlet;
 
 /**
+ * Application specific media types.
+ *
  * @author Thomas Segismont
  */
 public class CustomMediaTypes {
 
     public static final String APPLICATION_VND_RHQ_WRAPPED_JSON = "application/vnd.rhq.wrapped+json";
+
+    /**
+     * For JSONP.
+     * @see org.rhq.metrics.restServlet.jsonp.JsonPProvider
+     */
+    public static final String APPLICATION_JAVASCRIPT = "application/javascript";
 
     private CustomMediaTypes() {
         // Constants class
