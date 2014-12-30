fff8ff3102528bcf17baea3a801c27a74c1b2f76
Stefan Negrea <snegrea@redhat.com>
2014-12-30 14:41:01 -0600

    First cut at a better start.sh script that will not build from source for non-dev environments. Also aiming towards having to download just start.sh to get a full working environment built from public maven artifacts.

--- start.sh
+++ start.sh
@@ -4,9 +4,111 @@
 
 # set -x
 
-BACKEND=mem
-# uncomment the next line if you want to use an already running Cassandra server for the backend.
-# BACKEND=cass
+#========================================================================================
+# Description: Display an error message and abort the script.
+#========================================================================================
+abort()
+{
+   echo >&2
+   for ARG in "$@"; do
+      echo "$ARG" >&2
+      echo "">&2
+   done
+   exit 1
+}
+
+#========================================================================================
+# Description: Display usage information then abort the script.
+#========================================================================================
+usage()
+{
+      USAGE=$(
+cat << EOF
+USAGE:   start.sh OPTIONS
+   --backend=mem|cass                     [OPTIONAL]
+      Backend type to be used by the deployment. 'mem' uses the built-in memory enginer, 'cass' connects to a cassandra cluster.
+      Script default is 'mem'.
+   --version=version                      [OPTIONAL]
+      Released version to be downloaded and used, only applicable if this script is run in 'release' mode.
+      Script default is the latest published version.
+   --dev                                  [OPTIONAL]
+      Set script mode to dev: build from source and use the artifacts just built. 'version' will be ignored.
+EOF
+)
+   EXAMPLE=$(
+cat << EOF
+EXAMPLE:
+   start.sh --backend="cass" --mode="release"
+EOF
+)
+   abort "$@" "$USAGE" "$EXAMPLE"
+}
+
+#========================================================================================
+# Description: Validate and parse input arguments
+#========================================================================================
+parse_and_validate_options()
+{
+   BACKEND='mem'
+   VERSION=
+   DEV=false
+
+   short_options="h"
+   long_options="help,version:,backend:,dev"
+
+   PROGNAME=${0##*/}
+   ARGS=$(getopt -s bash --options $short_options --longoptions $long_options --name $PROGNAME -- "$@" )
+   eval set -- "$ARGS"
+
+   while true; do
+       case $1 in
+         -h|--help)
+            usage
+            ;;
+         --version)
+            shift
+            RELEASE_VERSION="$1"
+            shift
+            ;;
+         --backend)
+            shift
+            case "$1" in
+               "mem")
+                  BACKEND="mem"
+                  shift
+                  ;;
+               "cass")
+                  BACKEND="cass"
+                  shift
+                  ;;
+               "")
+                  BACKEND="cass"
+                  shift
+                  ;;
+               *)
+                  BACKEND="cass"
+                  shift
+                  ;;
+            esac
+            ;;
+         --dev)
+            DEV=true
+            shift
+            ;;
+         --)
+            shift
+            break
+            ;;
+         *)
+            usage
+            ;;
+       esac
+   done
+}
+
+
+parse_and_validate_options $@
+
 
 # By default bind user ports to all interfaces
 BIND_ADDR=0.0.0.0
@@ -30,20 +132,41 @@ then
     BIND_ADDR=$1
 fi
 
-WFLY_VERSION=`grep "<version.wildfly>" pom.xml | sed -E 's/^.*y>(8.*l)<.*$/\1/'`
+MVN_REPO=`mvn help:evaluate -Dexpression=settings.localRepository ${MVN_SETTINGS_OPT} | grep -vF "[INFO]" | tail -1`
 
-mvn install -DskipTests ${MVN_SETTINGS_OPT}
-if [ $? -ne 0 ]
+
+if [ $DEV == false ];
 then
-   exit;
+    MAVEN_DEPENDENCY_OUTPUT=`mvn dependency:list -f start.xml`
+
+    METRICS_CONSOLE_VERSION=`echo "${MAVEN_DEPENDENCY_OUTPUT}" | grep 'org.rhq.metrics:metrics-console:war' | cut -d':' -f4`
+    REST_SERVLET_VERSION=`echo "${MAVEN_DEPENDENCY_OUTPUT}" | grep 'org.rhq.metrics:rest-servlet:war' | cut -d':' -f4`
+    EXPLORER_VERSION=`echo "${MAVEN_DEPENDENCY_OUTPUT}" | grep 'org.rhq.metrics:explorer:war' | cut -d':' -f4`
+else
+    METRICS_CONSOLE_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
+    REST_SERVLET_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
+    EXPLORER_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
+
+    #mvn install -DskipTests ${MVN_SETTINGS_OPT}
+    if [ $? -ne 0 ]
+    then
+        exit;
+    fi
 fi
+
+
+echo ${METRICS_CONSOLE_VERSION}
+echo ${REST_SERVLET_VERSION}
+echo ${EXPLORER_VERSION}
 
 if [ ! -e target ]
 then
     mkdir target
 fi
 
-MVN_REPO=`mvn help:evaluate -Dexpression=settings.localRepository ${MVN_SETTINGS_OPT} | grep -vF "[INFO]" | tail -1`
+
+
+WFLY_VERSION=`grep "<version.wildfly>" pom.xml | sed -E 's/^.*y>(8.*l)<.*$/\1/'`
 WFLY_ZIP=${MVN_REPO}/org/wildfly/wildfly-dist/${WFLY_VERSION}/wildfly-dist-${WFLY_VERSION}.zip
 
 if [ ! -e target/wild* ]
@@ -51,9 +174,11 @@ then
     cd target
     unzip ${WFLY_ZIP}
     cd ..
+else
+    rm target/wildfly-${WFLY_VERSION}/standalone/deployments/*
 fi
 
-cp rest-servlet/target/rhq-metric-rest*.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
-cp ui/console/target/metrics-console-*.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
-cp ui/explorer/target/explorer-*.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
+cp ${MVN_REPO}/org/rhq/metrics/rest-servlet/${REST_SERVLET_VERSION}/rest-servlet-${REST_SERVLET_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
+cp ${MVN_REPO}/org/rhq/metrics/metrics-console/${METRICS_CONSOLE_VERSION}/metrics-console-${METRICS_CONSOLE_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
+cp ${MVN_REPO}/org/rhq/metrics/explorer/${EXPLORER_VERSION}/explorer-${EXPLORER_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
 target/wildfly-${WFLY_VERSION}/bin/standalone.sh -Drhq-metrics.backend=${BACKEND} --debug 8787 -b ${BIND_ADDR}
