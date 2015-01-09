43e49b910236043e209cc71d2459fdba516c62bd
Stefan Negrea <snegrea@redhat.com>
2015-01-08 21:17:55 -0600

    Revamp the start.sh script to avoid the requirement of having an external start.xml file.
    Also, added back the embedded cassandra option that was lost due to merge/rebase of the feature branch.

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-
-# Usage start.sh [mem|cass|em_cass] [bind_addr]
-
-# set -x
+#!/bin/bash -e
 
 #========================================================================================
 # Description: Display an error message and abort the script.
@@ -26,8 +22,11 @@ usage()
 cat << EOF
 USAGE:   start.sh OPTIONS
    --backend=mem|cass                     [OPTIONAL]
-      Backend type to be used by the deployment. 'mem' uses the built-in memory enginer, 'cass' connects to a cassandra cluster.
-      Script default is 'mem'.
+      Backend type to be used by the deployment.
+      'mem'          = use built-in memory enginer
+      'cass'         = connects to a cassandra cluster
+      'embedded_cass = use and connect to the built-in embedded cassandra server
+      Script default is 'embedded_cass'.
    --version=version                      [OPTIONAL]
       Released version to be downloaded and used, only applicable if this script is run in 'release' mode.
       Script default is the latest published version.
@@ -51,8 +50,9 @@ EOF
 #========================================================================================
 parse_and_validate_options()
 {
-   BACKEND='mem'
-   VERSION=
+   BACKEND="mem"
+   VERSION="0.2.4"
+   VERSION_REQUESTED=false
    DEV=false
    BIND_ADDRESS=0.0.0.0
 
@@ -71,6 +71,7 @@ parse_and_validate_options()
          --version)
             shift
             RELEASE_VERSION="$1"
+            VERSION_REQUESTED=true
             shift
             ;;
          --backend)
@@ -84,12 +85,16 @@ parse_and_validate_options()
                   BACKEND="cass"
                   shift
                   ;;
+               "embedded_cass")
+                  BACKEND="embedded_cass"
+                  shift
+                  ;;
                "")
-                  BACKEND="cass"
+                  BACKEND="embedded_cass"
                   shift
                   ;;
                *)
-                  BACKEND="cass"
+                  BACKEND="embedded_cass"
                   shift
                   ;;
             esac
@@ -114,9 +119,138 @@ parse_and_validate_options()
    done
 }
 
+#========================================================================================
+# Description: Prepares Wildfly archive
+#========================================================================================
+prepare_wildfly()
+{
+   local TEMPLATE=$(
+cat << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<project>
+  <modelVersion>4.0.0</modelVersion>
+
+  <groupId>org.rhq.metrics.build</groupId>
+  <artifactId>rhq-metrics-build</artifactId>
+  <version>0.0.1</version>
+  <packaging>pom</packaging>
+
+  <dependencies>
+    <dependency>
+      <groupId>org.wildfly</groupId>
+      <artifactId>wildfly-dist</artifactId>
+      <version>{version}</version>
+      <type>zip</type>
+    </dependency>
+  </dependencies>
+</project>
+EOF
+)
+
+   local WILDFLY_VERSION=$1
+
+   TEMPLATE=`echo $TEMPLATE | sed "s/{version}/$WILDFLY_VERSION/g"`
+
+   echo $TEMPLATE > wildfly.xml
+
+   mvn dependency:list -f wildfly.xml -q
+   if [ $? -ne 0 ]
+   then
+      clean_temp_files
+      echo "WILDFLY version $1 not found!"
+      exit 1
+   fi
+
+   clean_temp_files
+}
+
+#========================================================================================
+# Description: Creates a pom.xml file to download the project binaries
+#========================================================================================
+create_build_pom_file()
+{
+   local TEMPLATE=$(
+cat << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<project>
+  <modelVersion>4.0.0</modelVersion>
+
+  <groupId>org.rhq.metrics.build</groupId>
+  <artifactId>rhq-metrics-build</artifactId>
+  <version>0.0.1</version>
+  <packaging>pom</packaging>
+
+  <dependencies>
+    <dependency>
+      <groupId>org.rhq.metrics</groupId>
+      <artifactId>{dependency}</artifactId>
+      <version>{version}</version>
+      <type>{type}</type>
+    </dependency>
+  </dependencies>
+</project>
+EOF
+)
+
+   local DEPENDENCY=$1
+   local DEPENDENCY_VERSION=$2
+   local ARCHIVE_TYPE=$3
+
+   TEMPLATE=`echo $TEMPLATE | sed "s/{dependency}/$DEPENDENCY/g"`
+   TEMPLATE=`echo $TEMPLATE | sed "s/{version}/$DEPENDENCY_VERSION/g"`
+   TEMPLATE=`echo $TEMPLATE | sed "s/{type}/$ARCHIVE_TYPE/g"`
+
+   echo $TEMPLATE > start.xml
+}
+
+#========================================================================================
+# Description: Cleans temp files created by the script
+#========================================================================================
+clean_temp_files()
+{
+  rm -f output.txt
+  rm -f start.xml
+  rm -f widlfy.txt
+}
+
+#========================================================================================
+# Description: Downloads the right version of the project artifact
+#========================================================================================
+get_dependency()
+{
+   create_build_pom_file $1 $2 $3
+
+   if [ $VERSION_REQUESTED == false ]
+   then
+      mvn versions:use-latest-releases -DgenerateBackupPoms=false -f start.xml -q
+      if [ $? -ne 0 ]
+      then
+         clean_temp_files
+         echo "NOT_FOUND"
+         return
+      fi
+   fi
+
+   mvn dependency:list -f start.xml > output.txt
+   if [ $? -ne 0 ]
+   then
+      clean_temp_files
+      echo "NOT_FOUND"
+      return
+   fi
+
+   local POM_VERSION=`cat output.txt | grep "$4" | cut -d':' -f4`
+   clean_temp_files
+
+   echo $POM_VERSION
+}
+
+
+#========================================================================================
+# SCRIPT START
+#========================================================================================
 
 parse_and_validate_options $@
-
 
 # Use the path to maven settings.xml file from the environment if available
 if [ -n "${MVN_SETTINGS}" ]
@@ -126,48 +260,59 @@ else
     MVN_SETTINGS_OPT=""
 fi
 
-if [ x$1 == "xcass" -o x$1 == "xmem" ]
-then
-    BACKEND=$1
-    shift
-fi
-
-
 MVN_REPO=`mvn help:evaluate -Dexpression=settings.localRepository ${MVN_SETTINGS_OPT} | grep -vF "[INFO]" | tail -1`
 
 
 if [ $DEV == false ];
 then
-    MAVEN_DEPENDENCY_OUTPUT=`mvn dependency:list -f start.xml`
+   echo "Using: "
 
-    METRICS_CONSOLE_VERSION=`echo "${MAVEN_DEPENDENCY_OUTPUT}" | grep 'org.rhq.metrics:metrics-console:war' | cut -d':' -f4`
-    REST_SERVLET_VERSION=`echo "${MAVEN_DEPENDENCY_OUTPUT}" | grep 'org.rhq.metrics:rest-servlet:war' | cut -d':' -f4`
-    EXPLORER_VERSION=`echo "${MAVEN_DEPENDENCY_OUTPUT}" | grep 'org.rhq.metrics:explorer:war' | cut -d':' -f4`
+   METRICS_CONSOLE_VERSION=$(get_dependency "metrics-console" $VERSION "war" "org.rhq.metrics:metrics-console:war")
+   echo "RHQ Metrics Console: ${METRICS_CONSOLE_VERSION}"
+
+   REST_SERVLET_VERSION=$(get_dependency "rest-servlet" $VERSION "war" "org.rhq.metrics:rest-servlet:war")
+   echo "RHQ REST: ${REST_SERVLET_VERSION}"
+
+   EXPLORER_VERSION=$(get_dependency "explorer" $VERSION "war" "org.rhq.metrics:explorer:war")
+   echo "RHQ Explorer: ${EXPLORER_VERSION}"
+
+   EMBEDDED_CASSANDRA_VERSION=$(get_dependency "embedded-cassandra-ear" $VERSION "ear" "org.rhq.metrics:embedded-cassandra-ear:ear")
+   echo "Embedded Cassandra: ${EMBEDDED_CASSANDRA_VERSION}"
 else
-    METRICS_CONSOLE_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
-    REST_SERVLET_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
-    EXPLORER_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
+   echo "Using: "
 
-    mvn install -DskipTests ${MVN_SETTINGS_OPT}
-    if [ $? -ne 0 ]
-    then
-        exit;
-    fi
+   METRICS_CONSOLE_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
+   echo "RHQ Metrics Console: ${METRICS_CONSOLE_VERSION}"
+
+   REST_SERVLET_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
+   echo "RHQ REST: ${REST_SERVLET_VERSION}"
+
+   EXPLORER_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
+   echo "RHQ Explorer: ${EXPLORER_VERSION}"
+
+   EMBEDDED_CASSANDRA_VERSION=`grep "<version>" pom.xml | head -n 1| awk -F '(<|>)' '{print $3}'`
+   echo "Embedded Cassandra: ${EMBEDDED_CASSANDRA_VERSION}"
+
+   mvn install -DskipTests ${MVN_SETTINGS_OPT}
+   if [ $? -ne 0 ]
+   then
+      exit
+   fi
 fi
-
-echo 'Using: '
-echo 'RHQ Metrics Console: ${METRICS_CONSOLE_VERSION}'
-echo 'RHQ REST: ${REST_SERVLET_VERSION}'
-echo 'RHQ Explorer: ${EXPLORER_VERSION}'
 
 if [ ! -e target ]
 then
     mkdir target
 fi
 
+if [ $DEV == false ];
+then
+   WFLY_VERSION="8.2.0.Final"
+   prepare_wildfly $WFLY_VERSION
+else
+   WFLY_VERSION=`grep "<version.wildfly>" pom.xml | sed -E 's/^.*y>(8.*l)<.*$/\1/'`
+fi
 
-
-WFLY_VERSION=`grep "<version.wildfly>" pom.xml | sed -E 's/^.*y>(8.*l)<.*$/\1/'`
 WFLY_ZIP=${MVN_REPO}/org/wildfly/wildfly-dist/${WFLY_VERSION}/wildfly-dist-${WFLY_VERSION}.zip
 
 if [ ! -e target/wild* ]
@@ -179,7 +324,24 @@ else
     rm target/wildfly-${WFLY_VERSION}/standalone/deployments/*
 fi
 
-cp ${MVN_REPO}/org/rhq/metrics/rest-servlet/${REST_SERVLET_VERSION}/rest-servlet-${REST_SERVLET_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
-cp ${MVN_REPO}/org/rhq/metrics/metrics-console/${METRICS_CONSOLE_VERSION}/metrics-console-${METRICS_CONSOLE_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
-cp ${MVN_REPO}/org/rhq/metrics/explorer/${EXPLORER_VERSION}/explorer-${EXPLORER_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
+if [ "$REST_SERVLET_VERSION" != "NOT_FOUND" ]
+then
+   cp ${MVN_REPO}/org/rhq/metrics/rest-servlet/${REST_SERVLET_VERSION}/rest-servlet-${REST_SERVLET_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
+fi
+
+if [ "$METRICS_CONSOLE_VERSION" != "NOT_FOUND" ]
+then
+   cp ${MVN_REPO}/org/rhq/metrics/metrics-console/${METRICS_CONSOLE_VERSION}/metrics-console-${METRICS_CONSOLE_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
+fi
+
+if [ "$EXPLORER_VERSION" != "NOT_FOUND" ]
+then
+   cp ${MVN_REPO}/org/rhq/metrics/explorer/${EXPLORER_VERSION}/explorer-${EXPLORER_VERSION}.war target/wildfly-${WFLY_VERSION}/standalone/deployments/
+fi
+
+if [ "$EMBEDDED_CASSANDRA_VERSION" != "NOT_FOUND" ]
+then
+   cp ${MVN_REPO}/org/rhq/metrics/embedded-cassandra-ear/${EMBEDDED_CASSANDRA_VERSION}/embedded-cassandra-ear-${EXPLORER_VERSION}.ear target/wildfly-${WFLY_VERSION}/standalone/deployments/
+fi
+
 target/wildfly-${WFLY_VERSION}/bin/standalone.sh -Drhq-metrics.backend=${BACKEND} --debug 8787 -b ${BIND_ADDRESS}
