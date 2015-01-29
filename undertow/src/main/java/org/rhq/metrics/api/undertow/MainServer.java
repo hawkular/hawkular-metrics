59f1ebb03f7b5fca4735a3c445a429287ad9615d
Stefan Negrea <snegrea@redhat.com>
2015-01-29 17:22:44 -0600

    First attempt to implement a route in undertow.

--- undertow/src/main/java/org/rhq/metrics/api/undertow/MainServer.java
+++ undertow/src/main/java/org/rhq/metrics/api/undertow/MainServer.java
@@ -28,6 +28,17 @@ import io.undertow.util.HttpString;
 import io.undertow.util.Methods;
 
 import java.util.Date;
+import java.util.Deque;
+import java.util.Map;
+
+import org.rhq.metrics.RHQMetrics;
+import org.rhq.metrics.core.MetricId;
+import org.rhq.metrics.core.MetricsService;
+import org.rhq.metrics.core.NumericMetric;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 
 /**
  * Undertow app initialization
@@ -37,14 +48,46 @@ import java.util.Date;
 
 public class MainServer {
 
+    private static MetricsService metricsService;
+
     public static void main(final String[] args) {
+        metricsService = getMetricsService("mem");
+
         RoutingHandler commonHandler = Handlers.routing()
                 .add(Methods.GET, "/ping", new PingHandler())
                 .add(Methods.POST, "/ping", new PingHandler());
 
+        commonHandler.add(Methods.POST, "/{tenantId}/metrics/numeric", new HttpHandler() {
+            @Override
+            public void handleRequest(HttpServerExchange exchange) throws Exception {
+                Map<String, Deque<String>> queryParams = exchange.getQueryParameters();
+                System.out.println(queryParams.get("tenantId"));
+
+                String tenantId = queryParams.get("tenantId").getFirst();
+
+                // System.out.printl(exchange.getResponseChannel().)
+
+                String name = queryParams.get("name").getFirst();
+                Integer dataRetention = Integer.parseInt(queryParams.get("dataRetention").getFirst());
+
+                NumericMetric metric = new NumericMetric(tenantId, new MetricId(name), null, dataRetention);
+                ListenableFuture<Void> future = metricsService.createMetric(metric);
+                Futures.addCallback(future, new FutureCallback<Void>() {
+                    @Override
+                    public void onSuccess(Void result) {
+                        exchange.setResponseCode(200);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable t) {
+                        exchange.setResponseCode(501);
+                    }
+                });
+            }
+        });
+
         Undertow server = Undertow.builder().addHttpListener(8080, "0.0.0.0")
                 .setHandler(new CorsHandler(path().addPrefixPath("/rhq-metrics", commonHandler))).build();
-
         server.start();
     }
 
@@ -89,4 +132,31 @@ public class MainServer {
             }
         }
     }
+
+    public static MetricsService getMetricsService(String backend) {
+        if (metricsService == null) {
+            RHQMetrics.Builder metricsServiceBuilder = new RHQMetrics.Builder();
+
+            if (backend != null) {
+                switch (backend) {
+                case "cass":
+                    metricsServiceBuilder.withCassandraDataStore();
+                    break;
+                case "mem":
+                    metricsServiceBuilder.withInMemoryDataStore();
+                    break;
+                case "embedded_cass":
+                default:
+                    metricsServiceBuilder.withCassandraDataStore();
+                }
+            } else {
+                metricsServiceBuilder.withCassandraDataStore();
+            }
+
+            metricsService = metricsServiceBuilder.build();
+        }
+
+        return metricsService;
+    }
 }
+
