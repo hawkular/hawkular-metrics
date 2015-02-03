c9fd806b250ce73dbc6cace2047994ae82d80a08
Stefan Negrea <snegrea@redhat.com>
2015-02-03 16:32:33 -0600

    Port two sample methods from JAX-RS to undertow implementation.

---
+++ undertow/src/main/java/org/rhq/metrics/api/undertow/PingHandler.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2014-2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.rhq.metrics.api.undertow;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+
+import java.util.Date;
+
+class PingHandler implements HttpHandler {
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        exchange.getResponseSender().send((new Date()).toString());
+    }
+}
