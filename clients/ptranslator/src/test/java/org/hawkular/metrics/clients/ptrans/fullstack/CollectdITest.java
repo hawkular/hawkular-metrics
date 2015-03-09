4499eb7c9cdbdcb610bac9be966d19e87b58bbd4
Thomas Segismont <tsegismo@redhat.com>
2015-03-09 22:53:51 +0100

    Make Travis install collectd (and fail CollectdITest by default for now)

--- clients/ptranslator/src/test/java/org/hawkular/metrics/clients/ptrans/fullstack/CollectdITest.java
+++ clients/ptranslator/src/test/java/org/hawkular/metrics/clients/ptrans/fullstack/CollectdITest.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * Copyright 2014-2015 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,10 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.hawkular.metrics.clients.ptrans.fullstack;
 
 import static org.hawkular.metrics.clients.ptrans.ConfigurationKey.SERVICES;
+import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
 
 import java.io.FileInputStream;
@@ -48,7 +48,7 @@ public class CollectdITest extends ExecutableITestBase {
 
     @Test
     public void shouldFindCollectdMetricsOnServer() {
-
+        fail("Not implemented yet");
     }
 
     private void assumeCollectdIsPresent() {
