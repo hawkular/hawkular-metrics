ecd314e6d9fa9331646ca233df324f09b64ae3a4
Thomas Segismont <tsegismo@redhat.com>
2015-03-19 11:48:50 +0100

    Added computed end to Buckets class

--- core/metrics-core-api/src/main/java/org/hawkular/metrics/core/api/Buckets.java
+++ core/metrics-core-api/src/main/java/org/hawkular/metrics/core/api/Buckets.java
@@ -40,6 +40,7 @@ public final class Buckets {
     private final long start;
     private final long step;
     private final int count;
+    private final long end;
 
     public Buckets(long start, long step, int count) {
         checkArgument(start > 0, "start is not positive");
@@ -48,6 +49,7 @@ public final class Buckets {
         this.step = step;
         checkArgument(count > 0, "count is not positive");
         this.count = count;
+        end = start + count * step;
     }
 
     public long getStart() {
@@ -60,6 +62,17 @@ public final class Buckets {
 
     public int getCount() {
         return count;
+    }
+
+    /**
+     * Computed end. If this instance was created with {@link #fromCount(long, long, int)} or
+     * {@link #fromStep(long, long, long)}, then it could be different from the value specified as the {@code end}
+     * parameter of these factory methods.
+     *
+     * @return the computed end (start + count * step)
+     */
+    public long getEnd() {
+        return end;
     }
 
     @Override
