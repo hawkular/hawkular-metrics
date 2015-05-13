bd1674882ee980b56b12e6f25976cee118c3829e
Thomas Segismont <tsegismo@redhat.com>
2015-05-13 13:15:29 +0200

    HWKMETRICS-67 Influx endpoint: support pretty print query parameter
    PrettyFilter checks if the output should be formatted and PrettyInterceptor intercepts JSON response to format it.
    
    I've tried to find how to interact with the RestEasy Jackson mappers directly but the solution would have been hard to maintain.
    
    Here there's a little overhead due to parsing of the entity stream. But this will only be used by humans for a fraction of requests.

---
+++ api/metrics-api-jaxrs/src/main/java/org/hawkular/metrics/api/jaxrs/influx/prettyprint/PrettyFilter.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014-2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.metrics.api.jaxrs.influx.prettyprint;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Check if the request targets the Influx endpoint and the <em>pretty</em> query parameter is set to true. In this
+ * case, the {@link #PRETTY_PRINT} context property is set to {@link Boolean#TRUE}.
+ *
+ * @author Thomas Segismont
+ * @see PrettyInterceptor
+ */
+@Provider
+public class PrettyFilter implements ContainerRequestFilter {
+    static final String PRETTY_PRINT = PrettyFilter.class.getName();
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        UriInfo uriInfo = requestContext.getUriInfo();
+        if (!APPLICATION_JSON_TYPE.equals(requestContext.getMediaType())
+            || !uriInfo.getPath().startsWith("/db")) {
+            return;
+        }
+        requestContext.setProperty(PRETTY_PRINT, Boolean.valueOf(uriInfo.getQueryParameters().getFirst("pretty")));
+    }
+}
