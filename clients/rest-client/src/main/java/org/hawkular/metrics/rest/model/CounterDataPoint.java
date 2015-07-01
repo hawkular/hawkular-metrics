59b930620b806877dd26737fc9ad5f5ae68cd5e5
John Sanda <jsanda@redhat.com>
2015-07-01 16:29:16 -0400

    fix getters

---
+++ clients/rest-client/src/main/java/org/hawkular/metrics/rest/model/CounterDataPoint.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2014-2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.metrics.rest.model;
+
+import java.util.Objects;
+
+/**
+ * @author jsanda
+ */
+public class CounterDataPoint implements DataPoint<Long> {
+
+    private long timestamp;
+
+    private Long value;
+
+    private CounterDataPoint() {
+    }
+
+    public CounterDataPoint(long timestamp, Long value) {
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+
+    @Override
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public Long getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CounterDataPoint that = (CounterDataPoint) o;
+        return Objects.equals(timestamp, that.timestamp) &&
+                Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timestamp, value);
+    }
+
+    @Override
+    public String toString() {
+        return "CounterDataPoint{" +
+                "timestamp=" + timestamp +
+                ", value=" + value +
+                '}';
+    }
+}
