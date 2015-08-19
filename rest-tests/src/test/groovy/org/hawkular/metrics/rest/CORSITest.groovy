c4e144f9cc9738644ba9697ad2d93ff81375c977
Stefan Negrea <snegrea@redhat.com>
2015-08-19 17:20:53 -0500

    [HWKMETRICS-225] Add a simple test for CORS filters, the response status should always be 200, because the request should never reach the routers since this is a pre-flight call.

---
+++ rest-tests/src/test/groovy/org/hawkular/metrics/rest/CORSITest.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014-2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.metrics.rest
+
+import static org.junit.Assert.assertEquals
+
+import org.junit.Test
+
+class CORSITest extends RESTTest {
+
+    @Test
+    void testOptions() {
+        def response = hawkularMetrics.options(path: "ping",
+         headers: [
+            "Access-Control-Request-Method": "POST",
+            "origin": "test"
+         ]);
+
+         //Expected a 200 because this is be a pre-flight call that should never reach the resource router
+         assertEquals(200, response.status)
+    }
+}
