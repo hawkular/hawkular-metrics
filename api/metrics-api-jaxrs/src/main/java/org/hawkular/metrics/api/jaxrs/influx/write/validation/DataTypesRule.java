3cf5cc289d043e16d2d0d27f642f31f60c8d71b6
Thomas Segismont <tsegismo@redhat.com>
2015-09-16 13:32:19 +0200

    Simplify data checking code

--- api/metrics-api-jaxrs/src/main/java/org/hawkular/metrics/api/jaxrs/influx/write/validation/DataTypesRule.java
+++ api/metrics-api-jaxrs/src/main/java/org/hawkular/metrics/api/jaxrs/influx/write/validation/DataTypesRule.java
@@ -16,34 +16,14 @@
  */
 package org.hawkular.metrics.api.jaxrs.influx.write.validation;
 
-import static com.google.common.base.Predicates.and;
-import static com.google.common.base.Predicates.instanceOf;
-import static com.google.common.base.Predicates.not;
-import static com.google.common.base.Predicates.or;
-
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.ListIterator;
 
 import org.hawkular.metrics.api.jaxrs.influx.InfluxObject;
-
-import com.google.common.base.Predicate;
 
 /**
  * @author Thomas Segismont
  */
 public class DataTypesRule implements InfluxObjectValidationRule {
-    private static final Predicate<Object> IS_INTEGRAL = //
-    and( //
-        instanceOf(Number.class), //
-        not( //
-            or( //
-                instanceOf(BigDecimal.class), //
-                instanceOf(Double.class), //
-                instanceOf(Float.class) //
-            ) //
-        ) //
-    );
 
     @Override
     public void checkInfluxObject(InfluxObject influxObject) throws InvalidObjectException {
@@ -56,23 +36,9 @@ public class DataTypesRule implements InfluxObjectValidationRule {
             if (point.size() < columns.size()) {
                 throw new InvalidObjectException("Object has not enough data in point to match columns");
             }
-            if (columns.size() == 1) {
-                if (!(point.get(0) instanceof Number)) {
-                    throw new InvalidObjectException("Point 'value' is not numerical");
-                }
-            } else {
-                int valueIndex = columns.indexOf("value");
-                for (ListIterator<?> dataIterator = point.listIterator(); dataIterator.hasNext();) {
-                    Object data = dataIterator.next();
-                    if (valueIndex == dataIterator.nextIndex() - 1) {
-                        if (!(data instanceof Number)) {
-                            throw new InvalidObjectException("Point 'value' is not numerical");
-                        }
-                    } else {
-                        if (!IS_INTEGRAL.apply(data)) {
-                            throw new InvalidObjectException("Point 'time' is not integral");
-                        }
-                    }
+            for (Object data : point) {
+                if (!(data instanceof Number)) {
+                    throw new InvalidObjectException(data + " is not numerical");
                 }
             }
         }
