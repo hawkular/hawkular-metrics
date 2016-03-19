f2a46accac69c63da1b378c9cbd7748f1a0c0788
John Sanda <jsanda@redhat.com>
2016-03-19 13:43:48 -0400

    [HWKMETRICS-361] add bootstrap script to properly initialize schema
    The previous code in cassalog.groovy that had some conditional logic based on
    the version of Hawkular Metrics had some holes in the logic. bootstrap.groovy
    has been added to handle new installs and upgrades. Any schema changes can
    then be placed in cassalog.groovy without having to worry about whether it is
    a new install or an upgrade.

--- core/schema/src/main/java/org/hawkular/metrics/schema/SchemaService.java
+++ core/schema/src/main/java/org/hawkular/metrics/schema/SchemaService.java
@@ -16,14 +16,10 @@
  */
 package org.hawkular.metrics.schema;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
-import java.util.jar.Manifest;
 
 import org.cassalog.core.Cassalog;
 import org.cassalog.core.CassalogBuilder;
@@ -42,7 +38,7 @@ public class SchemaService {
         Map<String, ?> vars  = ImmutableMap.of(
                 "keyspace", keyspace,
                 "reset", resetDB,
-                "appVersion", getHawkularMetricsVersion()
+                "session", session
         );
         URI script = getScript();
 
@@ -54,23 +50,6 @@ public class SchemaService {
             return getClass().getResource("/cassalog.groovy").toURI();
         } catch (URISyntaxException e) {
             throw new RuntimeException("Failed to load schema change script", e);
-        }
-    }
-
-    private String getHawkularMetricsVersion() {
-        try {
-            Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
-            while (resources.hasMoreElements()) {
-                URL resource = resources.nextElement();
-                Manifest manifest = new Manifest(resource.openStream());
-                String vendorId = manifest.getMainAttributes().getValue("Implementation-Vendor-Id");
-                if (vendorId != null && vendorId.equals("org.hawkular.metrics")) {
-                    return manifest.getMainAttributes().getValue("Implementation-Version");
-                }
-            }
-            throw new RuntimeException("Unable to determine implementation version for Hawkular Metrics");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
