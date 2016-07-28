072855ba4c48e2abd578b74c120da3eddc4f8dfc
John Sanda <jsanda@redhat.com>
2016-07-27 22:09:39 -0400

    [HWKMETRICS-446] add debug logging to help track down failures on travis

--- core/metrics-core-service/src/main/java/org/hawkular/metrics/core/jobs/JobsServiceImpl.java
+++ core/metrics-core-service/src/main/java/org/hawkular/metrics/core/jobs/JobsServiceImpl.java
@@ -71,9 +71,9 @@ public class JobsServiceImpl implements JobsService {
     }
 
     @Override
-    public Single<JobDetails> submitDeleteTenantJob(String tenantId) {
-        return scheduler.scheduleJob(DeleteTenant.JOB_NAME, DeleteTenant.JOB_NAME, ImmutableMap.of("tenantId",
-                tenantId), new SingleExecutionTrigger.Builder().withDelay(5, TimeUnit.MINUTES).build());
+    public Single<JobDetails> submitDeleteTenantJob(String tenantId, String jobName) {
+        return scheduler.scheduleJob(DeleteTenant.JOB_NAME, jobName, ImmutableMap.of("tenantId", tenantId),
+                new SingleExecutionTrigger.Builder().withDelay(5, TimeUnit.MINUTES).build());
     }
 
 }
