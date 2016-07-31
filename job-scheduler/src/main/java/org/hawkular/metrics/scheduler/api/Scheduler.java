026ee66f631820c5df94b5a36a8d5a38c9007f7c
John Sanda <jsanda@redhat.com>
2016-07-31 16:46:03 -0400

    [HWKMETRICS-453] add retry policy for non-repeating jobs
    A retry policy specifies if and when a job should be retried in the event of a
    failure. Retry policies are effective only for non-repeating jobs. If a
    repating job fails, its trigger determines when it executes again. By default,
    failed jobs will not be retried.

--- job-scheduler/src/main/java/org/hawkular/metrics/scheduler/api/Scheduler.java
+++ job-scheduler/src/main/java/org/hawkular/metrics/scheduler/api/Scheduler.java
@@ -21,18 +21,54 @@ import java.util.Map;
 import rx.Completable;
 import rx.Single;
 import rx.functions.Func1;
+import rx.functions.Func2;
 
 /**
  * @author jsanda
  */
 public interface Scheduler {
 
+    /**
+     * Schedules a job for execution. A unique id is created and is persisted along with the specified job details.
+     * If the execution time has already passed, the job details will not be persisted, and the returned Single
+     * will call subscribers onError method.
+     *
+     * @param type
+     * @param name
+     * @param parameters
+     * @param trigger
+     * @return A Single that emits the job details
+     */
     Single<JobDetails> scheduleJob(String type, String name, Map<String, String> parameters, Trigger trigger);
 
-    void registerJobFactory(String jobType, Func1<JobDetails, Completable> factory);
+    /**
+     * Register a function that produces a job of the specified type. This method should be called prior to scheduling
+     * any jobs of the specified type.
+     *
+     * @param jobType
+     * @param jobProducer
+     */
+    void register(String jobType, Func1<JobDetails, Completable> jobProducer);
 
+    /**
+     * Registers two functions. The first produces a job of the specfied type. The second function returns a retry
+     * policy that is used with non-repeating jobs when the fail.
+     *
+     * @param jobType
+     * @param jobProducer
+     * @param retryFunction
+     */
+    void register(String jobType, Func1<JobDetails, Completable> jobProducer,
+            Func2<JobDetails, Throwable, RetryPolicy> retryFunction);
+
+    /**
+     * Start executing jobs.
+     */
     void start();
 
+    /**
+     * Shut down thread pools and stop executing jobs. Jobs that are running may be interrupted and might not finish.
+     */
     void shutdown();
 
 }
