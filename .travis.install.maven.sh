ec0ee6f5af79ead30b0069fa52fcdd7d06325970
Stefan Negrea <snegrea@redhat.com>
2016-10-11 15:02:13 -0500

    Try to fix the license header.

--- .travis.install.maven.sh
+++ .travis.install.maven.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
+# Copyright 2014-2016 Red Hat, Inc. and/or its affiliates
 # and other contributors as indicated by the @author tags.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
