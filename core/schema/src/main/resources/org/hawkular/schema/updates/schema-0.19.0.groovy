0462bf1a948358ed10fd141e1c15656775abe1f8
John Sanda <jsanda@redhat.com>
2016-11-08 11:17:49 -0500

    [HWKMETRICS-539] fix upgrade script

--- core/schema/src/main/resources/org/hawkular/schema/updates/schema-0.19.0.groovy
+++ core/schema/src/main/resources/org/hawkular/schema/updates/schema-0.19.0.groovy
@@ -15,14 +15,12 @@
  * limitations under the License.
  */
 
-setKeyspace keyspace
-
 schemaChange {
   version '3.0'
   author 'jsanda'
   tags '0.18.x', '0.19.x'
   cql """
-ALTER TABLE data WITH COMPRESSION = {'sstable_compression': 'DeflateCompressor'};
+ALTER TABLE ${keyspace}.data WITH COMPRESSION = {'sstable_compression': 'DeflateCompressor'};
 """
 }
 
