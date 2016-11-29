9c75623bfefaaf28f71aceacbf33923c5d61945a
John Sanda <jsanda@redhat.com>
2016-11-29 17:09:28 -0500

    [HWKMETRICS-526] test for new /admin/status endpoint

---
+++ integration-tests/rest-tests-jaxrs/src/test/groovy/org/hawkular/metrics/rest/AdminITest.groovy
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2014-2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.metrics.rest
+
+import org.junit.Test
+
+import static org.junit.Assert.*
+import static org.junit.Assume.assumeTrue
+/**
+ * @author jsanda
+ */
+class AdminITest extends RESTTest {
+
+  def tenantId = nextTenantId()
+
+  @Test
+  void getStatus() {
+    def version = System.properties.getProperty('project.version');
+    assumeTrue('This test only works in a Maven build', version != null)
+
+    def response = hawkularMetrics.get(path: "admin/status",
+        headers: [
+            (tenantHeaderName): tenantId,
+            (adminTokenHeaderName): adminToken
+        ]
+    )
+    assertEquals(200, response.status)
+
+    def expectedState = ['MetricsService': 'STARTED']
+
+    assertEquals(expectedState.MetricsService, response.data.MetricsService)
+    assertEquals(version, response.data['Implementation-Version'])
+    assertNotNull(response.data['Built-From-Git-SHA1'])
+    assertNotEquals('Unknown', response.data['Built-From-Git-SHA1'])
+
+    assertFalse(response.data.Cassandra.empty)
+    assertEquals([[address: 'localhost', status: 'up']], response.data.Cassandra)
+  }
+
+}
