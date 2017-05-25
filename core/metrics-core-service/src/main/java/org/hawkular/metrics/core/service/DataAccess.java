b42f67d52d395ebccae1393e7a2aa12fdd047237
John Sanda <jsanda@redhat.com>
2017-05-25 16:36:07 -0400

    [HWKMETRICS-660] use logged batches for adding tags
    This commit updates the methods for adding tags which is used by endpoints like
    
    PUT /hawkular/metrics/gauges/{id}/tags
    
    I did not update the method for creating metrics since it is called by
    Heapster. The create metric operation also has a flag that will cause it to
    use a light weight transaction. LWTs cannot be batched AFAIK so we will have
    think more about how we want to handle this, in a separate ticket though. I
    did also update the deleteMetric method as well to keep tags consistent there.

--- core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/DataAccess.java
+++ core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/DataAccess.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2016 Red Hat, Inc. and/or its affiliates
+ * Copyright 2014-2017 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,7 +18,6 @@
 package org.hawkular.metrics.core.service;
 
 import java.util.Map;
-import java.util.Set;
 
 import org.hawkular.metrics.core.service.compress.CompressedPointContainer;
 import org.hawkular.metrics.model.AvailabilityType;
@@ -55,7 +54,9 @@ public interface DataAccess {
 
     <T> Observable<ResultSet> addTags(Metric<T> metric, Map<String, String> tags);
 
-    <T> Observable<ResultSet> deleteTags(Metric<T> metric, Set<String> tags);
+    <T> Observable<ResultSet> deleteTags(Metric<T> metric, Map<String, String> tags);
+
+    <T> Observable<ResultSet> deleteFromMetricsIndexAndTags(MetricId<T> id, Map<String, String> tags);
 
     <T> Observable<Integer> updateMetricsIndex(Observable<Metric<T>> metrics);
 
@@ -104,10 +105,6 @@ public interface DataAccess {
             Map<String, Integer> retentions);
 
     <T> ResultSetFuture updateRetentionsIndex(Metric<T> metric);
-
-    <T> Observable<ResultSet> insertIntoMetricsTagsIndex(Metric<T> metric, Map<String, String> tags);
-
-    <T> Observable<ResultSet> deleteFromMetricsTagsIndex(Metric<T> metric, Map<String, String> tags);
 
     Observable<Row> findMetricsByTagName(String tenantId, String tag);
 
