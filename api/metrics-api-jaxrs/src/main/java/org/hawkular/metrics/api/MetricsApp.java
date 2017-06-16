85f165966acd12ad14ae557db74560e2aa9d17c6
Stefan Negrea <snegrea@redhat.com>
2017-06-16 17:11:45 -0500

    [HWKMETRICS-683] Initial work for the vert.x conversion using the framework from Hawkular Commons.

---
+++ api/metrics-api-jaxrs/src/main/java/org/hawkular/metrics/api/MetricsApp.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014-2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.metrics.api;
+
+import java.util.concurrent.ExecutorService;
+
+import org.hawkular.commons.log.MsgLogger;
+import org.hawkular.commons.log.MsgLogging;
+import org.hawkular.commons.properties.HawkularProperties;
+import org.hawkular.handlers.BaseApplication;
+
+public class MetricsApp implements BaseApplication {
+    private static final MsgLogger log = MsgLogging.getMsgLogger(MetricsApp.class);
+    private static final String BASE_URL = "hawkular-merics.base-url";
+    private static final String BASE_URL_DEFAULT = "/hawkular/metrics";
+
+    ExecutorService executor;
+    String baseUrl = HawkularProperties.getProperty(BASE_URL, BASE_URL_DEFAULT);
+
+    @Override
+    public void start() {
+        log.infof("Metrics app started on [ %s ] ", baseUrl());
+    }
+
+    @Override
+    public void stop() {
+        log.infof("Metrics app stopped", baseUrl());
+    }
+
+    @Override
+    public String baseUrl() {
+        return baseUrl;
+    }
+}
---
+++ api/metrics-api-jaxrs/src/main/java/org/hawkular/metrics/api/handler/PingHandler.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014-2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.metrics.api.handler;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.hawkular.handlers.RestEndpoint;
+import org.hawkular.handlers.RestHandler;
+import org.hawkular.metrics.api.JsonUtil;
+import org.hawkular.metrics.api.jaxrs.util.StringValue;
+
+import io.swagger.annotations.ApiOperation;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.rx.java.ObservableHandler;
+import io.vertx.rx.java.RxHelper;
+
+/**
+ * @author Stefan Negrea
+ */
+@RestEndpoint(path = "/ping")
+public class PingHandler implements RestHandler {
+
+    @Override
+    public void initRoutes(String baseUrl, Router router) {
+        router.get(baseUrl + "/ping").handler(ping().toHandler());
+    }
+
+    @ApiOperation(value = "Returns the current time and serves to check for the availability of the api.", response =
+            Map.class)
+    public ObservableHandler<RoutingContext> ping() {
+        ObservableHandler<RoutingContext> oh = RxHelper.observableHandler(true);
+        oh.subscribe(ctx -> ctx.response().end(JsonUtil.toJson(new StringValue(new Date().toString()))));
+        return oh;
+    }
+}
