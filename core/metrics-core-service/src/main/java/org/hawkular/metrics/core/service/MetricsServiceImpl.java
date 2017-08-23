3a790b0e5029c4239f1c84b2859b93e1c18363a3
Michael Burman <yak@iki.fi>
2017-08-23 14:51:17 +0300

    Remove one extra this::findMetric and replace casting to use default data retention

--- core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/MetricsServiceImpl.java
+++ core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/MetricsServiceImpl.java
@@ -195,6 +195,7 @@ public class MetricsServiceImpl implements MetricsService {
     private ExpressionTagQueryParser expresssionTagQueryParser;
 
     private int defaultTTL = Duration.standardDays(7).toStandardSeconds().getSeconds();
+    private int DEFAULT_RETENTION = (int) Duration.standardSeconds(defaultTTL).getStandardDays();
 
     private int maxStringSize;
 
@@ -533,8 +534,7 @@ public class MetricsServiceImpl implements MetricsService {
                 .filter(row -> tenantId.equals(row.getString(0)))
                 .compose(new MetricIdentifierFromFullDataRowTransformer(defaultTTL))
                 .distinct()
-                .flatMap(this::findMetric)
-                .map(m -> (Metric<T>) m);
+                .map(m -> new Metric(m, DEFAULT_RETENTION));
 
         if (metricType == null) {
             setFromMetricsIndex = Observable.from(MetricType.userTypes())
@@ -545,7 +545,7 @@ public class MetricsServiceImpl implements MetricsService {
             setFromMetricsIndex = dataAccess.findMetricsInMetricsIndex(tenantId, metricType)
                     .compose(new MetricsIndexRowTransformer<>(tenantId, metricType, defaultTTL));
 
-            setFromData = setFromData.filter(m -> metricType.equals(m.getMetricId().getType()));
+            setFromData = setFromData.filter(m -> metricType.equals(m.getType()));
         }
 
         return setFromMetricsIndex.concatWith(setFromData).distinct(Metric::getMetricId);
