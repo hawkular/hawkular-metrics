2111d964bf05a6e50cff90f3bc6fc8211d5cf2aa
Michael Burman <miburman@redhat.com>
2018-02-10 18:03:46 +0200

    Improve simple tag query performance by enabling the A-cost query possibilities
    Add better regexp detection and test for most common cases
    
    Add ^!.* as regexp check since we allow that syntax
    
    Optimize OR-queries also in the query optimizer
    
    Change tagNameAndValues to use IN

--- core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/MetricsServiceImpl.java
+++ core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/MetricsServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 Red Hat, Inc. and/or its affiliates
+ * Copyright 2014-2018 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -191,6 +191,7 @@ public class MetricsServiceImpl implements MetricsService {
     /**
      * Tools that do tag query parsing and execution
      */
+    private boolean disableACostOptimization;
     private SimpleTagQueryParser tagQueryParser;
     private ExpressionTagQueryParser expresssionTagQueryParser;
 
@@ -271,7 +272,7 @@ public class MetricsServiceImpl implements MetricsService {
 
         verifyAndCreateTempTables();
 
-        tagQueryParser = new SimpleTagQueryParser(this.dataAccess, this);
+        tagQueryParser = new SimpleTagQueryParser(this.dataAccess, this, disableACostOptimization);
         expresssionTagQueryParser = new ExpressionTagQueryParser(this.dataAccess, this);
     }
 
@@ -346,6 +347,7 @@ public class MetricsServiceImpl implements MetricsService {
         log.infoInsertRetryConfig(insertMaxRetries, insertRetryMaxDelay);
 
         defaultPageSize = Integer.parseInt(configuration.get("page-size", "5000"));
+        disableACostOptimization = Boolean.parseBoolean(configuration.get("disable.parser.optimization", "false"));
     }
 
     private void setDefaultTTL(Session session, String keyspace) {
