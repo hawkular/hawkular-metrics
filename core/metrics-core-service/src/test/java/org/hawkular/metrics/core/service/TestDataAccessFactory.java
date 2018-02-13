9aa767c42234fd43cc8035888f7c9d299ebd45f3
Michael Burman <yak@iki.fi>
2018-02-13 15:55:40 +0200

    Force keyspace change during TestDataAccessFactory initialization

--- core/metrics-core-service/src/test/java/org/hawkular/metrics/core/service/TestDataAccessFactory.java
+++ core/metrics-core-service/src/test/java/org/hawkular/metrics/core/service/TestDataAccessFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 Red Hat, Inc. and/or its affiliates
+ * Copyright 2014-2018 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,10 +23,20 @@ import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import org.hawkular.metrics.core.service.log.CoreLogger;
+import org.hawkular.metrics.core.service.log.CoreLogging;
 import org.hawkular.metrics.datetime.DateTimeService;
 import org.joda.time.DateTime;
 
+import com.datastax.driver.core.AggregateMetadata;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.FunctionMetadata;
+import com.datastax.driver.core.KeyspaceMetadata;
+import com.datastax.driver.core.MaterializedViewMetadata;
+import com.datastax.driver.core.SchemaChangeListener;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.TableMetadata;
+import com.datastax.driver.core.UserType;
 
 import rx.schedulers.Schedulers;
 
@@ -35,7 +45,10 @@ import rx.schedulers.Schedulers;
  */
 public class TestDataAccessFactory {
 
+    private static final CoreLogger log = CoreLogging.getCoreLogger(TestDataAccessFactory.class);
+
     public static DataAccess newInstance(Session session) {
+        session.execute(String.format("USE %s", BaseITest.getKeyspace()));
         final CountDownLatch latch = new CountDownLatch(3);
         final CountDownLatch fallBackTable = new CountDownLatch(0);
         DataAccessImpl dataAccess = new DataAccessImpl(session) {
@@ -43,6 +56,7 @@ public class TestDataAccessFactory {
             void prepareTempStatements(String tableName, Long mapKey) {
                 super.prepareTempStatements(tableName, mapKey);
                 if(DataAccessImpl.OUT_OF_ORDER_TABLE_NAME.equals(tableName)) {
+                    log.infof("data_0 statements being prepared");
                     fallBackTable.countDown();
                 }
                 if (latch.getCount() > 0) {
@@ -59,7 +73,6 @@ public class TestDataAccessFactory {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
         return dataAccess;
     }
 
