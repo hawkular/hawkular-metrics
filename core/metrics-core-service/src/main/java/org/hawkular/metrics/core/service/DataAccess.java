9e1f25fbac775b65a4c5675b16baa4a5da205886
Michael Burman <miburman@redhat.com>
2018-03-28 23:06:55 +0300

    compressBlock will check for all possible blocks to compress

--- core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/DataAccess.java
+++ core/metrics-core-service/src/main/java/org/hawkular/metrics/core/service/DataAccess.java
@@ -72,6 +72,8 @@ public interface DataAccess {
      */
 //    Completable resetTempTable(long timestamp);
 
+    Set<Long> findExpiredTables();
+
     Observable<Observable<Row>> findAllDataFromBucket(long timestamp, int pageSize, int maxConcurrency);
 
     Observable<ResultSet> dropTempTable(long timestamp);
